e842549b55fa737f509f8a1344784fe32068eb12
Jun Shao <mr.shaojun@gmail.com>
2021-07-08 16:07:20 +1000

    ui styling refine

--- zdn-web/craco.config.js
+++ zdn-web/craco.config.js
@@ -13,12 +13,14 @@ const darkTheme = getThemeVariables({
 const modifyVars = {
   ...darkTheme,
   '@primary-color': '#13c2c2', // rgb(19,194,194)
-  '@info-color': '#13c2c2',
+  '@info-color': '#1890ff',
   '@link-color': '#13c2c2',
-  '@success-color': '#13c2c2',
+  '@success-color': '#52c41a',
   '@font-size-base': '14px',
   '@table-font-size': '12px',
   '@layout-header-background': '#002329',
+  '@heading-color': '#002329',
+  '@label-color': '#00232988',
   // '@height-base': '32px',
   // '@height-lg': '40px',
   // '@height-sm': '24px',
