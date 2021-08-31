b74e3cb1977dd8b1062adb4fb8f2510e1918dc61
Jun Shao <mr.shaojun@gmail.com>
2021-08-31 20:50:58 +1000

    add service worker precache

--- zdn-web/src/index.js
+++ zdn-web/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 console.log(process.env);
 
@@ -17,7 +18,13 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
 
 Notification.requestPermission().then((result) => {
   if (result === 'granted') {
