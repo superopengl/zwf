30030308fbd0ac453a3c31dfa9cc0c0c76e224ec
Jun Shao <mr.shaojun@gmail.com>
2021-11-23 10:37:32 +1100

    improve forgot password workflow

--- zdn-app/src/utils/searchOrgClients.ts
+++ zdn-app/src/utils/searchOrgClients.ts
@@ -1,11 +1,5 @@
 import { getRepository } from 'typeorm';
 import { assert } from './assert';
-import { Subscription } from '../entity/Subscription';
-import { User } from '../entity/User';
-import { UserProfile } from '../entity/UserProfile';
-import { SubscriptionStatus } from '../types/SubscriptionStatus';
-import { Role } from '../types/Role';
-import { Org } from '../entity/Org';
 import { OrgClientInformation } from '../entity/views/OrgClientInformation';
 
 export type StockUserParams = {
