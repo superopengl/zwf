76e41de022ffb438af93c857e70cbc1d3d57be1a
Jun Shao <mr.shaojun@gmail.com>
2022-02-23 23:33:35 +1100

    task share link refine

--- zwf-app/src/api/taskDirectLinkHanlder.ts
+++ zwf-app/src/api/taskDirectLinkHanlder.ts
@@ -5,23 +5,48 @@ import * as httpAssert from 'http-assert';
 import { Task } from '../entity/Task';
 import { getUserIdFromReq } from '../utils/getUserIdFromReq';
 import { handlerWrapper } from '../utils/asyncHandler';
+import { assertRole } from '../utils/assertRole';
+import { getOrgIdFromReq } from '../utils/getOrgIdFromReq';
+import { assert } from '../utils/assert';
 
 export const taskDirectLinkHanlder = handlerWrapper(async (req, res) => {
   const token = req.params.token;
   const role = getRoleFromReq(req);
-  if (role === Role.Client) {
-    const userId = getUserIdFromReq(req);
-    const task = await getRepository(Task).findOne({
-      deepLinkId: token,
-      userId
-    })
-    httpAssert(task, 404);
-    // Redirect to a logged in task page
-    res.redirect(`${process.env.ZWF_WEB_DOMAIN_NAME}/task/${task.id}`);
-  } else if (role === Role.Guest) {
-    // Redirect to a task deep page without login
-    res.redirect(`${process.env.ZWF_WEB_DOMAIN_NAME}/task/direct/${token}`);
-  } else {
-    httpAssert(false, 404, 'Task direct link is for anonymous user or client user. You need to logout logout before accessing it.');
+
+  let query: any = {
+    deepLinkId: token,
+  };
+
+  switch (role) {
+    case Role.Admin:
+    case Role.Agent:
+      const orgId = getOrgIdFromReq(req);
+      query = {
+        ...query,
+        orgId,
+      }
+      break;
+    case Role.Client:
+      const userId = getUserIdFromReq(req);
+      query = {
+        ...query,
+        userId
+      }
+      break;
+    case Role.Guest:
+      // Redirect to a task deep page without login
+      res.redirect(`${process.env.ZWF_WEB_DOMAIN_NAME}/task/direct/${token}`);
+    case Role.System:
+      assert(false, 403, 'Task direct link is for org, client and guest users.')
+    default:
+      assert(false, 404);
   }
+
+  const task = await getRepository(Task).findOne({
+    where: query,
+    select: ['id']
+  })
+  httpAssert(task, 404);
+  // Redirect to a logged in task page
+  res.redirect(`${process.env.ZWF_WEB_DOMAIN_NAME}/task/${task.id}`);
 });
