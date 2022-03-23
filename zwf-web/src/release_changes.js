43db8d6275844748ac04f40f7299f4e34efa0cea
Jun Shao <mr.shaojun@gmail.com>
2022-03-23 21:26:55 +1100

    name fix

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,13 +1,4 @@
-export const tag='Release-20220323-1245'
 export const notes=`
-don't talk with oneself
-homepage hooks
-fix unique email address
-code clean up
-refine contact
-supporter view fix
-support chat message count fix
- support refine
-ui refine
-chat ux refine
-contact bugfix`
+fix org initial creation
+styling refine
+refine release`
