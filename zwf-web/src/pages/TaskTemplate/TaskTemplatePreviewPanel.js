2d32bd55266a0227a849165f9ca040261604ddaf
Jun Shao <mr.shaojun@gmail.com>
2022-04-29 10:12:12 +1000

    misc fix

--- zwf-web/src/pages/TaskTemplate/TaskTemplatePreviewPanel.js
+++ zwf-web/src/pages/TaskTemplate/TaskTemplatePreviewPanel.js
@@ -32,6 +32,7 @@ export const TaskTemplatePreviewPanel = props => {
       <TaskFormWidget 
         fields={taskTemplate.fields}
         docs={taskTemplate.docs}
+        hasDoc={taskTemplate.hasDoc}
         type={type}
         mode="create"
       />
