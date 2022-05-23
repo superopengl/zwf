c6642df4519de1f828a5588ca8912a620571e617
Jun Shao <mr.shaojun@gmail.com>
2022-05-23 17:13:32 +1000

    downgrade to react 17
    Because antd components frozen in React 18

--- zwf-web/craco.config.js
+++ zwf-web/craco.config.js
@@ -54,4 +54,14 @@ module.exports = {
       },
     },
   ],
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          'react/jsx-runtime': 'react/jsx-runtime.js',
+          'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+        },
+      },
+    }
+  }
 };
