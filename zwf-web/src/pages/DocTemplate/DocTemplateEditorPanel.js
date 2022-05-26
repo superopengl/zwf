9a77c56520ad356e664057e970e588ac26d03d4e
Jun Shao <mr.shaojun@gmail.com>
2022-05-26 22:13:42 +1000

    doc template refine

--- zwf-web/src/pages/DocTemplate/DocTemplateEditorPanel.js
+++ zwf-web/src/pages/DocTemplate/DocTemplateEditorPanel.js
@@ -4,7 +4,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Typography, Button, Alert, Input, Modal, Form, Tooltip, Tag, Drawer, Radio } from 'antd';
-import {RichTextInput} from 'components/RichTextInput';
+import { RichTextInput } from 'components/RichTextInput';
 
 const Container = styled.div`
   margin: 0 auto 0 auto;
@@ -32,12 +32,18 @@ export const DocTemplateEditorPanel = props => {
         onValuesChange={(changedValues, allValues) => onChange(allValues)}
         initialValues={entity}
         style={{ position: 'relative' }}>
-        <Form.Item name="description" label="Description" rules={[{ required: true, message: ' ' }]}>
-          <Input.TextArea allowClear autoSize={{ minRows: 3 }} placeholder="Doc template description. This will be shown on the create task wizard to help users fill required fields to generate this document." />
-        </Form.Item>
-        <Form.Item name="html" label="Body" rules={[{ required: true, message: ' ' }]}>
+        <Form.Item name="html" rules={[{
+          required: true, 
+          message: 'Doc template content is empty',
+          validator: async (rule, value) => { 
+            debugger;
+          }
+        }]}>
           <RichTextInput />
         </Form.Item>
+        {/* <Form.Item name="description" label="Notes" rules={[{ required: false, message: ' ' }]}>
+          <Input.TextArea allowClear autoSize={{ minRows: 3 }} placeholder="Doc template description or extra information. This won't be display to the end clients." />
+        </Form.Item> */}
       </Form>
     </Container >
   );
