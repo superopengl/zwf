dc82a67478a62f84b1f2c0291665f259c41b5a97
Jun Shao <mr.shaojun@gmail.com>
2022-05-27 09:57:12 +1000

    clean up emails

--- zwf-app/src/types/EmailTemplateType.ts
+++ zwf-app/src/types/EmailTemplateType.ts
@@ -7,35 +7,17 @@ export enum EmailTemplateType {
   InviteClientUser = 'invite_client_user',
   ResetPassword = 'reset_password',
 
-  // Authorize
-  RequireClientAuthorizing = 'require_client_auth',
-
-  // Recurring
-  RecurringDone = 'recurring_done',
-  RecurringFailed = 'recurring_failed',
-
   // Task
   TaskCreated = 'task_created',
   TaskCompleted = 'task_completed',
-  TaskArchived = 'task_archived',
-  TaskSigned = 'task_signed',
-  TaskRequireSign = 'task_require_sign',
   TaskRequireAction = 'task_require_action',
-  TaskStatusChange = 'task_status_change',
-  TaskMessageNotification = 'task_message_notification',
-  TaskCommentNotification = 'task_comment_notification',
 
   // Subscription and payment
   SubscriptionCreated = 'subscription_created',
   SubscriptionExpiring = 'subscription_expiring',
   SubscriptionExpired = 'subscription_expired',
-  AutoRenewPaymentSuccessful = 'auto_renew_payment_successful',
-  AutoRenewPaymentFailed = 'auto_renew_payment_failed',
-  PaymentSuccessfulAndReceipt = 'payment_successful_and_receipt',
-
-  // System
-  Contact = 'contact',
-  SystemNotification = 'system_notification',
+  SubscriptionPaymentSuccessful = 'subscription_payment_successful',
+  SubscriptionPaymentFailed = 'subscription_payment_failed',
 }
 
 
