b1e210945cc343fdb87c8ebd381e0a4f3505e7ce
Jun Shao <mr.shaojun@gmail.com>
2022-05-30 16:17:02 +1000

    update release notes

--- zwf-app/src/utils/generatePdfBufferFromHtml.ts
+++ zwf-app/src/utils/generatePdfBufferFromHtml.ts
@@ -2,6 +2,7 @@ import * as puppeteer from 'puppeteer';
 
 export async function generatePdfBufferFromHtml(html) {
   const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome',
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     headless: true
   })
