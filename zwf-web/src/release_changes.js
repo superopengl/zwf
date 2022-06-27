40fa30dcea5d03af67ecbf5f70b9c32d646018e8
Jun Shao <mr.shaojun@gmail.com>
2022-06-27 13:47:01 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,2 +1,9 @@
 export const notes=`
-adjust doc template preview modal width`
+refine logos
+fix reset password default connection issue
+extract subject from mjml title
+MJML email templates
+welcome-org email template in MJML
+try send out right away when enqueue an email
+clean up
+typo`
