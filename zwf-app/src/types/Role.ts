d0d33d0c2fc8b5461347676fd2fdd088e4ea9616
Jun Shao <mr.shaojun@gmail.com>
2022-07-03 14:45:59 +1000

    add admin_expired and agent_expired roles

--- zwf-app/src/types/Role.ts
+++ zwf-app/src/types/Role.ts
@@ -2,6 +2,8 @@ export enum Role {
     System = 'system',  // Highest permission
     Admin = 'admin',    // Org admin (root user)
     Agent = 'agent',    // Org member
+    AdminExpired = 'admin_expired', // Org admin without alive subscription
+    AgentExpired= 'agent_expired', // Org member without alive subscription
     Client = 'client',  // Client
     Guest = 'guest'     // Anonymous user
 }
