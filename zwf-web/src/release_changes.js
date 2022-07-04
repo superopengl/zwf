644f9b028d92a02328d651c314bfd99ebd249c35
Jun Shao <mr.shaojun@gmail.com>
2022-07-04 12:48:17 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,3 +1,10 @@
 export const notes=`
-update release notes
-fix invoice logo`
+federal login test
+set orgId when sign up org trial user
+send email when register with an existing email
+daily subscription app
+add admin_expired and agent_expired roles
+typeorm fix
+add bulk enqueue email function
+update typeorm usage
+fix table entity`
