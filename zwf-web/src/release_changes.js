83734c3637802b1126638ed3d77e8a1fe7f91a4c
Jun Shao <mr.shaojun@gmail.com>
2022-07-24 17:35:02 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,8 +1,8 @@
 export const notes=`
-UX refine for smart search
-go to / for unknown route path
-clean up
-more rxjs usage
-code clean up
-save task template bugfix
-save doc template bugfix`
+simplify base64 image upload
+fix rich text editor base64 image upload
+fix doc template editor
+tweak text editor height
+remove wangeditor-for-react
+tinymce rich text editor
+tweak font size`
