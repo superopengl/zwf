fa78e56506e556973e6800b6db3634575c51c256
Jun Shao <mr.shaojun@gmail.com>
2022-09-01 09:27:07 +1000

    albe to google sso to register org

--- zwf-web/src/components/OrgRegisterInput.js
+++ zwf-web/src/components/OrgRegisterInput.js
@@ -1,59 +1,36 @@
 import React from 'react';
-import { Typography, Space, Input } from 'antd';
-import { PlusOutlined, UploadOutlined } from '@ant-design/icons';
-import Icon, {
-  ClockCircleOutlined, SettingOutlined, TeamOutlined,
-  BankOutlined, QuestionOutlined, FileOutlined, TagsOutlined, MailOutlined
-} from '@ant-design/icons';
-import { FormattedMessage } from 'react-intl';
-import { Modal } from 'antd';
-import OrgSignUpForm from 'pages/Org/OrgSignUpForm';
+import { Typography, Input } from 'antd';
+import { OrgRegisterModal } from './OrgRegisterModal';
 
 const { Text } = Typography;
 
 export const OrgRegisterInput = React.memo(props => {
 
-  const [loading, setLoading] = React.useState(false);
   const [visible, setVisible] = React.useState(false);
-  const [email, setEmail] = React.useState('');
 
-
-  const handleClick = (value) => {
-    const inputEmail = value.trim();
-    if (inputEmail) {
-      setEmail(inputEmail);
-      setVisible(true);
-      setLoading(true);
-    }
+  const handleShowModal = () => {
+    setVisible(true);
   }
 
-  const handleReset = () => {
+  const handleHideModal = () => {
     setVisible(false);
-    setEmail('');
-    setLoading(false)
   }
 
 
   return <>
     <Input.Search
-      style={{maxWidth: '500px', }}
+      style={{ maxWidth: '500px', }}
       size="large"
-      // prefix={<MailOutlined />}
-      value={email}
-      onChange={e => setEmail(e.target.value)}
+      onClick={handleShowModal}
       placeholder="Email"
       enterButton="Try it Now"
-      loading={loading}
-      onSearch={handleClick}
+      onSearch={handleShowModal}
     />
-    <Modal
+    <OrgRegisterModal
       visible={visible}
-      destroyOnClose
-      onCancel={handleReset}
-      footer={null}
-    >
-      <OrgSignUpForm value={email} onOk={handleReset} />
-    </Modal>
+      onOk={handleHideModal}
+      onCancel={handleHideModal}
+    />
   </>
 
 });
---
+++ zwf-web/src/components/OrgRegisterModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Typography } from 'antd';
+import { Modal } from 'antd';
+import OrgSignUpForm from 'pages/Org/OrgSignUpForm';
+
+const { Text } = Typography;
+
+export const OrgRegisterModal = React.memo(props => {
+
+  const { visible, onOk, onCancel } = props;
+
+
+  return <Modal
+    title="Org Registration"
+    visible={visible}
+    onCancel={onCancel}
+    destroyOnClose
+    maskClosable={false}
+    closable={true}
+    footer={null}
+  >
+    <OrgSignUpForm onOk={onOk} />
+  </Modal>
+
+});
+
+
