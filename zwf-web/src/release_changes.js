b0cb95dcb2a0b21fb23077147011c33dda9db8a3
Jun Shao <mr.shaojun@gmail.com>
2022-09-06 17:57:47 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,13 +1,2 @@
 export const notes=`
-contact us section
-functional Join Now button
-feature section
-menu to pricing section
-refine pricing section
-refine resources page
-ui refine for footer
-apply UX to resource page
-fix promotion code
-ui refine
-remove domain field from Org
-style refine`
+homepage refine`
