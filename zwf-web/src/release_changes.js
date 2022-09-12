5a0af6990b3386a1c799ad50411dc7c193d84850
Jun Shao <mr.shaojun@gmail.com>
2022-09-12 11:47:08 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,23 +1,2 @@
 export const notes=`
-small submit button
-landing page refine
-restyle footer
-more homepage feature styling
-resource page new styling
-more subscription refine
-more tests
-add test for calcSubscriptionBlockPayment
-add tests
-refactoring
-code refactoring around payment
-add test for refunding
-refactoring
-fix renaming
-code refactoring
-add subscription flow logic in readme
-code refactoring around payment
-subscription cron refine
-fix cross route anchor scrolling
-larger font in resource page
-refine light1 and light2
-guest contact feature`
+change logo`
