a81db58afa911c9c9f14b54e6fef64a92d961e96
Jun Shao <mr.shaojun@gmail.com>
2022-09-15 10:21:11 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,9 +1,2 @@
 export const notes=`
-update release notes
-homepage ui refine
-homepage refine
-tweak content padding
-fix notify action email
-fix mjml var typo
-send out invite client email when create task for new email
-fix client download file to be signed`
+update release notes`
