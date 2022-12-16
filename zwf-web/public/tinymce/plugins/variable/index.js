79bd441ab6825bfbaa401d1ffcba3982137cce0c
Jun Shao <mr.shaojun@gmail.com>
2022-12-16 23:47:18 +1100

    update release notes

--- zwf-web/public/tinymce/plugins/variable/index.js
+++
@@ -1,7 +0,0 @@
-// Exports the "autolink" plugin for usage with module loaders
-// Usage:
-//   CommonJS:
-//     require('tinymce/plugins/autovar')
-//   ES2015:
-//     import 'tinymce/plugins/autovar'
-require('./plugin.js');
