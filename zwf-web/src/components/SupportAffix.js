fcd1c6c353fdf9f96ba965cd7e6f50e0ae1cd178
Jun Shao <mr.shaojun@gmail.com>
2023-01-25 15:00:59 +1100

    improve support affix

--- zwf-web/src/components/SupportAffix.js
+++ zwf-web/src/components/SupportAffix.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Affix, Space, Button, Card, Typography, Badge } from 'antd';
+import { Affix, Space, Button, Card, Typography, Badge, FloatButton } from 'antd';
 import { AiOutlineDown } from "react-icons/ai";
-import { MdMessage } from 'react-icons/md';
+import { MdOutlinePrivacyTip } from 'react-icons/md';
 import styled from 'styled-components';
 import { getMySupport$, subscribeSupportMessage, nudgeMyLastReadSupportMessage$ } from 'services/supportService';
 import { finalize, catchError } from 'rxjs/operators';
@@ -10,6 +10,8 @@ import { SupportMessageInput } from './SupportMessageInput';
 import { sendContact$ } from 'services/supportService';
 import { GlobalContext } from 'contexts/GlobalContext';
 import { getUserDisplayName } from 'util/getUserDisplayName';
+import { CloseOutlined, CommentOutlined, CustomerServiceOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+
 
 const { Paragraph, Title } = Typography;
 
@@ -49,6 +51,8 @@ width: 400px;
 
 export const SupportAffix = () => {
   const [visible, setVisible] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [chatOpen, setChatOpen] = React.useState(false);
   const visibleRef = React.useRef(visible);
   const [loading, setLoading] = React.useState(true);
   const [unreadCount, setUnreadCount] = React.useState(0);
@@ -83,7 +87,7 @@ export const SupportAffix = () => {
     const sub$ = getMySupport$().pipe(
       finalize(() => setLoading(false))
     ).subscribe(resp => {
-      const {list, unreadCount} = resp;
+      const { list, unreadCount } = resp;
       setList(list);
       setUnreadCount(unreadCount);
     });
@@ -92,9 +96,9 @@ export const SupportAffix = () => {
   }, []);
 
   React.useEffect(() => {
-    if(visible && list?.length) {
+    if (visible && list?.length) {
       const lastMessage = list[list.length - 1];
-      const {id} = lastMessage;
+      const { id } = lastMessage;
       nudgeMyLastReadSupportMessage$(id).pipe(
         catchError()
       ).subscribe();
@@ -113,33 +117,69 @@ export const SupportAffix = () => {
     return sendContact$(message, capturedUrl);
   }
 
+  const handleShowChat = () => {
+    setChatOpen(true)
+    setOpen(false);
+  }
+
+  const handleFloatButtonOpenChange = open => {
+    setOpen(open);
+  }
+
   return <>
-    <Affix style={{ position: 'fixed', bottom: 30, right: 30, zIndex: 900 }}>
+    <FloatButton.Group
+      trigger="click"
+      type="primary"
+      style={{ right: 30, bottom: 30 }}
+      icon={<CustomerServiceOutlined />}
+      open={open}
+      onOpenChange={handleFloatButtonOpenChange}
+    >
+      <FloatButton
+        tooltip="Terms and Conditions"
+        icon={<QuestionCircleOutlined />}
+        href="/terms_and_conditions"
+        target="_blank"
+      />
+      <FloatButton
+        tooltip="Privacy Policy"
+        icon={<MdOutlinePrivacyTip />}
+        href="/privacy_policy"
+        target="_blank"
+      />
+      <FloatButton
+        icon={<CommentOutlined />}
+        tooltip="Chat with ZeeWorkflow support"
+        onClick={handleShowChat}
+      />
+    </FloatButton.Group>
+
+    {chatOpen && <Affix style={{ position: 'fixed', bottom: 30, right: 30, zIndex: 900 }}>
       <Space direction="vertical" style={{ alignItems: 'flex-end' }} size="large" >
-        {visible &&
-          <StyledCard
-            title={<>
-              <Title>👋 {cheerName}</Title>
-              <Paragraph type="secondary" style={{color: '#FFFFFF'}}>
-                Ask us anything, or share your feedback.
-              </Paragraph>
-            </>}
-            headStyle={{ backgroundColor: '#0FBFC4' }}
-            bodyStyle={{ padding: 0 }}
-          >
-            <div style={{ height: '50vh', minHeight: 200, maxHeight: 600 }}>
-              <SupportMessageList dataSource={list} loading={loading} />
-            </div>
-            <hr />
-            <SupportMessageInput loading={loading} onSubmit={handleSubmitMessage} />
-          </StyledCard>}
-        <Badge count={unreadCount} showZero={false}>
+        <StyledCard
+          title={<>
+            <Title>👋 {cheerName}</Title>
+            <Paragraph type="secondary" style={{ color: '#FFFFFF' }}>
+              Ask us anything, or share your feedback.
+            </Paragraph>
+          </>}
+          extra={<Button icon={<CloseOutlined />} onClick={() => setChatOpen(false)} type="text" />}
+          headStyle={{ backgroundColor: '#0FBFC4' }}
+          bodyStyle={{ padding: 0 }}
+        >
+          <div style={{ height: '50vh', minHeight: 200, maxHeight: 600 }}>
+            <SupportMessageList dataSource={list} loading={loading} />
+          </div>
+          <hr />
+          <SupportMessageInput loading={loading} onSubmit={handleSubmitMessage} />
+        </StyledCard>
+        {/* <Badge count={unreadCount} showZero={false}>
           <AffixContactButton type="primary" size="large" onClick={() => setVisible(x => !x)}>
-            {visible ? <AiOutlineDown size={24} /> : <MdMessage size={24} />}
+            <AiOutlineDown size={24} />
           </AffixContactButton>
-        </Badge>
+        </Badge> */}
       </Space>
-    </Affix>
+    </Affix>}
   </>
 }
 
