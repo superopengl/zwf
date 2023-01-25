dedaeec576f9e1ee0aee7af90840d8461821a401
Jun Shao <mr.shaojun@gmail.com>
2023-01-25 11:17:54 +1100

    antd pro component PageContainer

--- zwf-web/src/pages/OrgAccount/OrgPaymentMethodPage.js
+++ zwf-web/src/pages/OrgAccount/OrgPaymentMethodPage.js
@@ -1,38 +1,154 @@
+import { Card, Button, Modal, Space, Typography, Tag, List } from 'antd';
 import React from 'react';
 
-import styled from 'styled-components';
 import { Loading } from 'components/Loading';
-import OrgPaymentMethodPanel from './OrgPaymentMethodPanel';
-import { listMySubscriptions$ } from 'services/subscriptionService';
-import { finalize } from 'rxjs/operators';
+import { CloseCircleFilled, CloseOutlined, PlusOutlined } from '@ant-design/icons';
+import { deleteOrgPaymentMethod$, listOrgPaymentMethods$, setOrgPrimaryPaymentMethod$ } from 'services/orgPaymentMethodService';
+import StripeCardPaymentWidget from 'components/checkout/StripeCardPaymentWidget';
+import { saveOrgPaymentMethod } from 'services/orgPaymentMethodService';
+// import { PageContainer } from '@ant-design/pro-layout';
+import { PageContainer, ProCard, ProList } from '@ant-design/pro-components';
 
-const ContainerStyled = styled.div`
-  margin: 0 auto;
-  width: 100%;
-  max-width: 1200px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-contents: center;
-
-
-  .ant-spin-nested-loading {
-    width: 100%;
-  }
-
-  // .ant-divider {
-  //   margin: 20px 0 8px;
-  // }
-`;
-
+const { Text } = Typography;
 
 const OrgPaymentMethodPage = () => {
 
+  const [loading, setLoading] = React.useState(true);
+  const [list, setList] = React.useState([]);
+
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [paymentLoading, setPaymentLoading] = React.useState(false);
+
+  const load$ = () => {
+    setLoading(true);
+    return listOrgPaymentMethods$()
+      .subscribe(list => setList(list))
+      .add(() => setLoading(false));
+  }
+
+  React.useEffect(() => {
+    const sub$ = load$();
+    return () => {
+      sub$?.unsubscribe();
+    }
+  }, []);
+
+  const handleSavePayment = async (stripePaymentMethodId) => {
+    await saveOrgPaymentMethod(stripePaymentMethodId);
+    setModalVisible(false);
+    load$();
+  }
+
+  const hideModal = () => {
+    setModalVisible(false);
+  }
+
+  const handleAddNew = () => {
+    setModalVisible(true);
+  }
+
+  const handleDelete = (item) => {
+    Modal.confirm({
+      title: 'Remove payment method',
+      content: <>Delete card <Text code>{item.cardLast4}</Text>?</>,
+      closable: true,
+      maskClosable: true,
+      onOk: () => {
+        deleteOrgPaymentMethod$(item.id).subscribe(() => {
+          load$();
+        })
+      },
+      okText: 'Yes, delete it',
+      okButtonProps: {
+        danger: true
+      }
+    })
+  }
+
+  const handleSetPrimary = (item) => {
+    Modal.confirm({
+      title: 'Set primary payment method',
+      content: <>Set card <Text code>{item.cardLast4}</Text> as primary payment method for future payment?</>,
+      closable: true,
+      maskClosable: true,
+      onOk: () => {
+        setOrgPrimaryPaymentMethod$(item.id).subscribe(() => {
+          load$();
+        })
+      },
+      okText: 'Yes, use this',
+      okButtonProps: {
+        type: 'primary'
+      }
+    })
+  }
 
   return (
-    <ContainerStyled>
-      <OrgPaymentMethodPanel />
-    </ContainerStyled>
+    <PageContainer
+      loading={loading}
+      fixedHeader
+      header={{
+        title: 'Payment Methods',
+        extra: [
+          <Button key="add" type="primary" ghost icon={<PlusOutlined />} onClick={() => handleAddNew()}>Add New Method</Button>
+        ]
+      }}
+    >
+      <ProList
+        loading={loading}
+        dataSource={list}
+        split={true}
+        rowKey="id"
+        ghost={true}
+        // toolBarRender={() => <Button key="add" type="primary" ghost icon={<PlusOutlined />} onClick={() => handleAddNew()}>Add New Method</Button>}
+        grid={{ gutter: 16, column: 2 }}
+        metas={{
+          title: {
+            render: (value, item) => <Text strong={item.primary}>
+              XXXX-XXXX-XXXX-{item.cardLast4}
+            </Text>
+          },
+          content: {
+            render: (value, item) => <Space size="large">
+              <Text>{item.cardBrand}</Text>
+              <Text>{item.cardExpiry}</Text>
+            </Space>
+          },
+          // subTitle: {
+          //   render: (value, item) => item.primary ? <Tag color="#0FBFC4">being used</Tag> : null
+          // },
+          actions: {
+            render: (value, item) => item.primary ? [
+              <Tag key="tag" color="#0FBFC4">being used</Tag>
+            ] : [
+              <Button key="primary" type="text" onClick={() => handleSetPrimary(item)} size="small">Set Primary</Button>,
+              <Button key="delete" type="text" danger onClick={() => handleDelete(item)} size="small">Remove</Button>
+            ]
+          }
+        }}
+      />
+
+      <Modal
+        open={modalVisible}
+        closable={!paymentLoading}
+        maskClosable={false}
+        title="Add Payment Method"
+        destroyOnClose
+        footer={null}
+        width={460}
+        onOk={hideModal}
+        onCancel={hideModal}
+      >
+        <Loading loading={paymentLoading}>
+          <StripeCardPaymentWidget
+            onOk={handleSavePayment}
+            onLoading={loading => setPaymentLoading(loading)}
+            buttonText="Add this card"
+          />
+
+        </Loading>
+      </Modal>
+    </PageContainer>
   );
 };
 
