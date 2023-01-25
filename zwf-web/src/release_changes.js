40cbdbcf66d1d2e8b886b772938a2d1703b61498
Jun Shao <mr.shaojun@gmail.com>
2023-01-25 21:27:23 +1100

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,2 +1,23 @@
 export const notes=`
-home page fix`
+schemaForm for org profile
+fix dup key error
+fix pageContainer
+improve support affix
+fix PageContainer usage
+clean up
+clean up
+Use Segmented components
+agent is able to create task template
+add back support button
+more PageContainer fix
+responsive layout
+antd pro component PageContainer
+rxjs new syntax fix
+misc fix
+fix menu
+use pro-layout
+tweak style
+set theme color in antd v5 way
+upgrade antd and pro-components
+upgrade to React@18
+split to /invoices and /payment_method pages`
