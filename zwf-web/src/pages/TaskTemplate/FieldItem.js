479a938e13957ab9a48db87b1910ee04e9c6b3d9
Jun Shao <mr.shaojun@gmail.com>
2023-01-30 23:21:40 +1100

    dnd task template field editor

---
+++ zwf-web/src/pages/TaskTemplate/FieldItem.js
@@ -0,0 +1,102 @@
+import { useRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
+import PropTypes from 'prop-types';
+import { Card } from 'antd';
+import { ProCard } from '@ant-design/pro-components';
+
+const style = {
+  // border: '1px dashed gray',
+  // padding: '0.5rem 1rem',
+  // marginBottom: '.5rem',
+  // backgroundColor: 'white',
+  cursor: 'move',
+}
+
+export const FieldItem = (props) => {
+  const { value, index, onMove } = props;
+  const { id, name, type } = value;
+
+
+  const ref = useRef(null)
+  const [{ handlerId }, drop] = useDrop({
+    accept: 'field',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset()
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      // Time to actually perform the action
+      onMove(dragIndex, hoverIndex)
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    },
+  })
+  const [{ isDragging }, drag] = useDrag({
+    type: 'field',
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+  return <ProCard ref={ref}
+    data-handler-id={handlerId}
+    title={name}
+    size="small"
+    bordered
+    style={{ ...style, opacity }}>
+    <div  >
+      {type} {id} {name}
+    </div>
+  </ProCard>
+}
+
+
+FieldItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  value: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+
+};
+
+FieldItem.defaultProps = {
+  onChange: () => { },
+  onDelete: () => { },
+};
