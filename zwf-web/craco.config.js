c46f9c30959c5f4506bbb575c5b30ae8445799cc
Jun Shao <mr.shaojun@gmail.com>
2023-02-13 10:20:14 +1100

    respect index.less

--- zwf-web/craco.config.js
+++ zwf-web/craco.config.js
@@ -1,30 +1,30 @@
 const CracoLessPlugin = require('craco-less');
 
 module.exports = {
-  // plugins: [
-  //   {
-  //     plugin: CracoLessPlugin,
-  //     options: {
-  //       lessLoaderOptions: {
-  //         // javascriptEnabled: true,
-  //         // modifyVars,
-  //         lessOptions: {
-  //           javascriptEnabled: true,
-  //           // modifyVars,
-  //         }
-  //       }
-  //     },
-  //   },
-  // ],
-  // webpack: {
-  //   // This webpack config is for React 17. May consider removing once upgrade to React 18
-  //   configure: {
-  //     resolve: {
-  //       fallback: {
-  //         'react/jsx-runtime': 'react/jsx-runtime.js',
-  //         'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
-  //       },
-  //     },
-  //   }
-  // }
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          // javascriptEnabled: true,
+          // modifyVars,
+          lessOptions: {
+            javascriptEnabled: true,
+            // modifyVars,
+          }
+        }
+      },
+    },
+  ],
+  webpack: {
+    // This webpack config is for React 17. May consider removing once upgrade to React 18
+    configure: {
+      resolve: {
+        fallback: {
+          'react/jsx-runtime': 'react/jsx-runtime.js',
+          'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+        },
+      },
+    }
+  }
 };
