54162880f064667b7c126a2a08bfbeb6aa743b44
Jun Shao <mr.shaojun@gmail.com>
2023-03-04 13:59:13 +1100

    icon fix

--- zwf-web/src/components/entityIcon.js
+++ zwf-web/src/components/entityIcon.js
@@ -1,15 +1,13 @@
-import { Typography } from 'antd';
 
 import React from 'react';
 
-import Icon, { BorderOutlined, FileOutlined, FilePdfFilled, FilePdfOutlined, UserOutlined } from '@ant-design/icons';
+import Icon, { BorderOutlined, UserOutlined } from '@ant-design/icons';
 import { FaTasks } from 'react-icons/fa';
-import { ImInsertTemplate } from 'react-icons/im';
 import PropTypes from 'prop-types';
-import { FileIcon } from './FileIcon';
 import { MdOutlinePages } from 'react-icons/md';
+import { MdSpaceDashboard } from 'react-icons/md';
+import { BsFileEarmarkTextFill } from 'react-icons/bs';
 
-const { Text, Paragraph, Link: TextLink } = Typography;
 
 export const EntityIcon = React.memo(props => {
   const {icon, color, style} = props;
@@ -36,8 +34,8 @@ EntityIcon.defaultProps = {
 };
 
 export const TaskIcon = (props) => <EntityIcon icon={<FaTasks />} color="#0FBFC4" style={props.style} /> 
-export const TaskTemplateIcon = (props) => <EntityIcon icon={<ImInsertTemplate />} color="#9254de" style={props.style} /> 
-export const DocTemplateIcon = (props) => <EntityIcon icon={<FilePdfFilled />} color="#cf222e" style={props.style} /> 
+export const TaskTemplateIcon = (props) => <EntityIcon icon={<Icon component={MdSpaceDashboard} />} color="#9254de" style={props.style} /> 
+export const DocTemplateIcon = (props) => <EntityIcon icon={<Icon component={BsFileEarmarkTextFill} />} color="#cf222e" style={props.style} /> 
 export const ResourcePageIcon = (props) => <EntityIcon icon={<MdOutlinePages />} color="#13c2c2" style={props.style} /> 
 export const ClientIcon = (props) => <EntityIcon icon={<UserOutlined />} color="#00232944" style={props.style} /> 
 
