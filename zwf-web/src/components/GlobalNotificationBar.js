949fc33aed50e768ff5a7cb287f82ff924fc4ce2
Jun Shao <mr.shaojun@gmail.com>
2023-03-12 23:34:24 +1100

    Period ending notification and no payment method notification

--- zwf-web/src/components/GlobalNotificationBar.js
+++ zwf-web/src/components/GlobalNotificationBar.js
@@ -2,51 +2,73 @@ import React from 'react';
 import { message, Typography, Button, Space } from 'antd';
 import { listOrgPaymentMethods$ } from 'services/orgPaymentMethodService';
 import { useAddPaymentMethodModal } from '../hooks/useAddPaymentMethodModal';
-import { delay } from 'rxjs';
+import { delay, tap, of, filter, switchMap, forkJoin } from 'rxjs';
+import { useAuthUser } from 'hooks/useAuthUser';
+import moment from 'moment';
+import { notify } from 'util/notify';
+import { notification } from 'antd';
 
-const { Link } = Typography;
+const { Link, Paragraph, Text } = Typography;
 
-const MESSAGE_KEY = 'no-payment-method-notification';
-
+const PAYMENT_METHOD_NOT_SPECIFIED_KEY= 'Payment method not specified';
 export const GlobalNotificationBar = () => {
-  const [messageApi, messageContextHolder] = message.useMessage();
   const [openAddPaymentModal, modalContextHolder] = useAddPaymentMethodModal();
+  const [user] = useAuthUser();
+
+  const shouldTimeToPrompt = (periodTo) => {
+    return periodTo.add(-7, 'days').isBefore();
+  }
 
   React.useEffect(() => {
-    const $sub = listOrgPaymentMethods$()
-      .pipe(
-        delay(4000)
-      )
-      .subscribe(list => {
-        if (!list.length) {
-          showNotification();
-        }
-      });
+    const { suspended, currentPlanType, currentPeriodTo, role } = user;
+    const periodTo = moment(currentPeriodTo);
+    const trialPeriodCheck$ = of(null).pipe(
+      filter(() => currentPlanType === 'trial'),
+      tap(() => {
+        notify.info(
+          'Trial period is ending',
+          <>
+            <Paragraph>
+              The trial plan is ending on <Text underline>{periodTo.format('MMM D YYYY')}</Text>. And after then system will automatically transition to a monthly plan. The payment for the first monthly plan period will be processed on or after <Text underline>{periodTo.add(1, 'month').add(-1, 'day').format('MMM D YYYY')}</Text>.
+            </Paragraph>
+          </>,
+          0,
+        );
+      })
+    );
 
-    return () => $sub.unsubscribe();
-  }, []);
+    const paymentMethodCheck$ = of(null).pipe(
+      switchMap(() => listOrgPaymentMethods$()),
+      filter(list => !list?.length),
+      tap(() => {
+        notify.info(
+          PAYMENT_METHOD_NOT_SPECIFIED_KEY,
+          <>
+            <Paragraph>
+              To ensure the success of your business, please <Link strong onClick={handleAddPaymentMethod}>add a payment method</Link> as soon as possible, as none has been specified.
+            </Paragraph>
+          </>,
+          0,
+        );
+      })
+    );
 
-  const showNotification = () => {
-    messageApi.warning({
-      key: MESSAGE_KEY,
-      // icon: <ExclamationCircleOutlined />,
-      className: 'global-notification-bar',
-      content: <Space>
-        To ensure the success of your business, please <Link strong onClick={handleAddPaymentMethod}>add a payment method</Link> as soon as possible, as none has been specified.
-        <Button type="primary" onClick={handleAddPaymentMethod}>Add Payment Method</Button>
-      </Space>,
-      duration: 0,
-    })
-  }
+    const sub$ = forkJoin([trialPeriodCheck$, paymentMethodCheck$]).pipe(
+      filter(() => role === 'admin'),
+      filter(() => shouldTimeToPrompt(periodTo)),
+      delay(5000),
+    ).subscribe();
+
+    return () => sub$.unsubscribe();
+  }, [user]);
 
   const handleAddPaymentMethod = () => {
     openAddPaymentModal(() => {
-      messageApi.destroy(MESSAGE_KEY)
+      notification.destroy(PAYMENT_METHOD_NOT_SPECIFIED_KEY)
     })
   }
 
   return <>
-    {messageContextHolder}
     {modalContextHolder}
   </>;
 };
