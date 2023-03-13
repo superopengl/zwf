e0403ebd546e454c5fde5c51811bab2462f19b75
Jun Shao <mr.shaojun@gmail.com>
2023-03-13 23:23:02 +1100

    usage refine

--- zwf-web/src/pages/OrgSubscription/OrgLicenseUsagePanel.js
+++ zwf-web/src/pages/OrgSubscription/OrgLicenseUsagePanel.js
@@ -95,70 +95,84 @@ export const OrgLicenseUsagePanel = () => {
         </Col>
         <Col flex="auto">
           <ProCard gutter={[20, 20]} ghost direction='column'>
-            <ProCard title="Billing period">
-              <ClockCircleOutlined /> {moment(period.periodFrom).format('MMM DD YYYY')} - <ClockCircleOutlined /> {moment(period.periodTo).format('MMM DD YYYY')} ({period.periodDays} days)
-            </ProCard>
+
             {/* <DebugJsonPanel value={period} /> */}
-            <ProCard title="Payment" extra={period.payment && <Button type="link" target="_blank" icon={<DownloadOutlined />} href={getInvoiceUrl(period.payment.invoiceFileId)}>Download Invoice</Button>}>
+            <ProCard            >
+              <Row justify="space-between" style={{ alignItems: 'end' }}>
+                <Col>
+                  <Title>ALL IN ONE PLAN</Title>
+                </Col>
+                <Col>
+                  <Space style={{ alignItems: 'end' }}>
+                    <MoneyAmount value={period.promotionPlanPrice ?? period.planFullPrice} strong style={{ color: '#0FBFC4', fontSize: 46 }} />
+                    <Text type="secondary" style={{ position: 'relative', bottom: 12 }}>/ 1 user per month</Text>
+                  </Space>
+                </Col>
+              </Row>
+            </ProCard>
+            <ProCard
+              title="Billing"
+              extra={period.payment && <Button type="link" target="_blank" icon={<DownloadOutlined />} href={getInvoiceUrl(period.payment.invoiceFileId)}>Download Invoice</Button>}
+            >
+              <div style={{ marginBottom: 16 }}>
+                <ClockCircleOutlined /> {moment(period.periodFrom).format('MMM DD YYYY')} - <ClockCircleOutlined /> {moment(period.periodTo).format('MMM DD YYYY')} ({period.periodDays} days)
+              </div>
               {period.type === 'trial' ? <>
                 <Paragraph>
                   Free for unlimited accounts during 14 Day Free Trial period.
                 </Paragraph>
                 <Paragraph>
-                <Text strong>What will happen after the trial period ends?</Text><br/>
-                Upon the conclusion of the trial period, the system will automatically transition to a monthly plan at a rate of AUD $39.00 per account per month (unless an alternative, agreed-upon price is in effect). The payment for the monthly plan will be processed on or after the last day of the period. For example, if a trial period ends on <Text underline>{moment(period.periodTo).format('MMM D YYYY')}</Text>, the first monthly plan will begin around <Text underline>{moment(period.periodTo).add(1, 'day').format('MMM D YYYY')}</Text>, and the initial payment will be processed around <Text underline>{moment(period.periodTo).add(1, 'month').add(-1, 'day').format('MMM D YYYY')}</Text>. Please note that the payment may be processed later than expected due to various reasons.
+                  <Text strong>What will happen after the trial period ends?</Text><br />
+                  Upon the conclusion of the trial period, the system will automatically transition to a monthly plan at a rate of AUD $39.00 per account per month (unless an alternative, agreed-upon price is in effect). The payment for the monthly plan will be processed on or after the last day of the period. For example, if a trial period ends on <Text underline>{moment(period.periodTo).format('MMM D YYYY')}</Text>, the first monthly plan will begin around <Text underline>{moment(period.periodTo).add(1, 'day').format('MMM D YYYY')}</Text>, and the initial payment will be processed around <Text underline>{moment(period.periodTo).add(1, 'month').add(-1, 'day').format('MMM D YYYY')}</Text>. Please note that the payment may be processed later than expected due to various reasons.
                 </Paragraph>
-              </> :
-                !period.payment ? <>
-                  <Paragraph>
-                    Pending Payment. The subsequent automatic deduction is scheduled after <Text strong>
-                      <ClockCircleOutlined /> {moment(period.periodTo).format('MMM DD YYYY')}
-                    </Text>.
-                    Please kindly ensure that the primary payment method is valid and has enough balance.
-                  </Paragraph>
-                  {period.promotionCode && <Alert
-                    showIcon
-                    type="success"
-                    message="Discount eligiable"
-                    description={<>Congratulations! Your organization is eligible for a discounted rate of <MoneyAmount value={period.promotionPlanPrice} strong underline /> per person-month, as opposed to the regular price of  <MoneyAmount value={period.planFullPrice} delete />.</>} />}
-                </> : <>
-                  <Row wrap={false} gutter={20} justify="space-between">
-                    <Col span={8}>
-                      <Space direction='vertical'>
-                        <MoneyAmount value={period.payment?.payable} strong style={{ fontSize: 32, whiteSpace: 'nowrap' }} />
-                        {period.promotionCode && 'Discount rate'}
+              </> : period.payment ? <Row wrap={false} gutter={20} justify="space-between" >
+                <Col span={8}>
+                  <Space direction='vertical'>
+                    <MoneyAmount value={period.payment?.payable} strong style={{ fontSize: 32, whiteSpace: 'nowrap' }} />
+                    {period.promotionCode && 'Discount rate'}
+                  </Space>
+                </Col>
+                <Col>
+                  <Descriptions column={2}>
+                    <Descriptions.Item label="Paid at">
+                      {moment(period.checkoutDate).format('MMM DD YYYY')}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Paid with">
+                      Card ending with {period.payment.cardLast4}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Charged amount">
+                      <Space>
+                        <MoneyAmount value={period.payment.amount} delete={period.promotionCode} />
+                        {period.promotionCode && <MoneyAmount value={period.payment.payable} strong />}
                       </Space>
-                    </Col>
-                    <Col>
-                      <Descriptions column={2}>
-                        <Descriptions.Item label="Paid at">
-                          {moment(period.checkoutDate).format('MMM DD YYYY')}
-                        </Descriptions.Item>
-                        <Descriptions.Item label="Paid with">
-                          Card ending with {period.payment.cardLast4}
-                        </Descriptions.Item>
-                        <Descriptions.Item label="Charged amount">
-                          <Space>
-                            <MoneyAmount value={period.payment.amount} delete={period.promotionCode} />
-                            {period.promotionCode && <MoneyAmount value={period.payment.payable} strong />}
-                          </Space>
-                        </Descriptions.Item>
-                        <Descriptions.Item label="Plan price">
-                          <Space>
-                            <MoneyAmount value={period.planFullPrice} delete={period.promotionCode} postfix="/ mo" />
-                            {period.promotionCode && <MoneyAmount value={period.promotionPlanPrice} strong postfix="/ mo" />}
-                          </Space>
-                        </Descriptions.Item>
-                        <Descriptions.Item label="Used units (user-days)">
-                          {period.payment.payableDays}
-                        </Descriptions.Item>
-                        {period.promotionCode && <Descriptions.Item label="Promotion code">
-                          {period.promotionCode}
-                        </Descriptions.Item>}
-                      </Descriptions>
-                    </Col>
-                  </Row>
-                </>}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Plan price">
+                      <Space>
+                        <MoneyAmount value={period.planFullPrice} delete={period.promotionCode} postfix="/ mo" />
+                        {period.promotionCode && <MoneyAmount value={period.promotionPlanPrice} strong postfix="/ mo" />}
+                      </Space>
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Used units (user-days)">
+                      {period.payment.payableDays}
+                    </Descriptions.Item>
+                    {period.promotionCode && <Descriptions.Item label="Promotion code">
+                      {period.promotionCode}
+                    </Descriptions.Item>}
+                  </Descriptions>
+                </Col>
+              </Row> : <>
+                <Paragraph>
+                  Pending Payment. The subsequent automatic deduction is scheduled after <Text strong>
+                    <ClockCircleOutlined /> {moment(period.periodTo).format('MMM DD YYYY')}
+                  </Text>.
+                  Please kindly ensure that the primary payment method is valid and has enough balance.
+                </Paragraph>
+                {period.promotionCode && <Alert
+                  showIcon
+                  type="success"
+                  message="Discount eligiable"
+                  description={<>Congratulations! Your organization is eligible for a discounted rate of <MoneyAmount value={period.promotionPlanPrice} strong underline /> per person-month, as opposed to the regular price of  <MoneyAmount value={period.planFullPrice} delete />.</>} />}
+              </>}
             </ProCard>
             <ProCard title="Usage" extra={<Tooltip title={<>
               The calculation of total usage during the billing period is based on user-days, representing the number of active members in your organization. To learn more about the usage calculation algorithm, click on this icon.
