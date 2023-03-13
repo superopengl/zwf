fcd5f2bbce9bb8249d366e6642cd2d06a5be84f8
Jun Shao <mr.shaojun@gmail.com>
2023-03-14 09:16:39 +1100

    refine suspended check

--- zwf-app/src/utils/assertRole.ts
+++ zwf-app/src/utils/assertRole.ts
@@ -4,37 +4,14 @@ import { getOrgIdFromReq } from './getOrgIdFromReq';
 import { getRoleFromReq } from './getRoleFromReq';
 
 
-export function assertRole(req, roles, options = { ignoreSuspendCheck: false }) {
-  const reqRole = getRoleFromReq(req);
-  assert(req?.user || reqRole === Role.Guest, 401, 'Session timeout');
+export function assertRole(req, roles) {
+  const role = getRoleFromReq(req);
+  const orgId = getOrgIdFromReq(req);
 
-  const beingSuspended = req.user?.suspended;
-  if (roles && roles.length) {
-    const reqOrgId = getOrgIdFromReq(req);
-    let canAccess = false;
-    for (const allowedRole of roles) {
-      switch (allowedRole) {
-        case Role.Admin:
-        case Role.Agent:
-          // Org members must have orgId.
-          canAccess = reqRole === allowedRole && !!reqOrgId && (options.ignoreSuspendCheck || !beingSuspended);
-          break;
-        case Role.System:
-        case Role.Client:
-        case Role.Guest:
-        default:
-          // Non org members must not have orgId.
-          canAccess = reqRole === allowedRole && !reqOrgId;
-          break;
-      }
+  const hasOrg = !!orgId;
+  const mustHaveOrgRole = role === Role.Admin || role === Role.Agent;
+  const matchesOrg = hasOrg === mustHaveOrgRole;
 
-      if (canAccess) {
-        break;
-      }
-    }
-
-    assert(!beingSuspended, 423, `The organization has been suspended. Please get in touch with your organization's owner to request for it to be unlocked.`);
-    assert(canAccess, 403, `Invalid permission ('${reqRole}' is to access '${roles.join()}')`);
-  }
+  assert(roles.includes(role) && matchesOrg, 403, `Invalid permission ('${role}' is to access '${roles.join()}')`);
 }
 
