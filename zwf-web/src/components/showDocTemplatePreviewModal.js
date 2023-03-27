79858206288637ef9924f4ec33b862957965c68c
Jun Shao <mr.shaojun@gmail.com>
2023-03-27 21:55:03 +1100

    preview doc template

--- zwf-web/src/components/showDocTemplatePreviewModal.js
+++ zwf-web/src/components/showDocTemplatePreviewModal.js
@@ -1,43 +1,52 @@
 import React from 'react';
 
-import { Modal } from 'antd';
+import { Modal, Skeleton, Tag } from 'antd';
 
 import { DocTemplatePreviewPanel } from 'components/DocTemplatePreviewPanel';
 import { DocTemplateIcon } from './entityIcon';
+import { getDocTemplate$ } from 'services/docTemplateService';
 
 
-export function showDocTemplatePreviewModal(docTemplate, options = {}) {
-  if (!docTemplate) {
-    throw new Error('docTemplate is null');
-  }
-  const { onClose, allowTest, varBag, type } = options;
-  const modalRef = Modal.info({
-    title: <><DocTemplateIcon />{docTemplate.name}</>,
-    content: <>
-      <DocTemplatePreviewPanel
-        style={{ marginTop: 20 }}
-        value={docTemplate}
-        debug={false}
-        type={type || 'agent'}
-        allowTest={allowTest}
-        varBag={varBag || {}}
-      />
-    </>,
-    afterClose: () => {
-      onClose?.();
-    },
-    icon: null,
-    closable: true,
-    maskClosable: true,
-    destroyOnClose: true,
-    footer: null,
-    width: 800,
-    style: { top: 20 },
-    okButtonProps: {
-      style: {
-        display: 'none'
-      }
+export function useDocTemplatePreviewModal() {
+  const [modal, contextHolder] = Modal.useModal()
+
+  const open = (docTemplateId, name) => {
+
+    if (!docTemplateId) {
+      throw new Error('docTemplateId is null');
     }
-  });
-  return modalRef;
+
+    const modalInstance = modal.info({
+      title: <><DocTemplateIcon /> {name} <Tag color="warning">pending generation</Tag></>,
+      content: <Skeleton />,
+      icon: null,
+      closable: true,
+      maskClosable: true,
+      destroyOnClose: true,
+      footer: null,
+      width: 800,
+      style: { top: 20 },
+      okButtonProps: {
+        style: {
+          display: 'none'
+        }
+      },
+      zIndex: 4000,
+    });
+
+    getDocTemplate$(docTemplateId)
+      .subscribe((docTemplate) => {
+        modalInstance.update({
+          content: <DocTemplatePreviewPanel
+            style={{ marginTop: 20 }}
+            value={docTemplate}
+            debug={false}
+            allowTest={false}
+            varBag={{}}
+          />
+        });
+      });
+  }
+
+  return [open, contextHolder];
 }
