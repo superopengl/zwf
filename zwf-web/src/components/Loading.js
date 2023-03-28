2bb04f691c7ff4b3ec9cd823f02b565f57bc7d33
Jun Shao <mr.shaojun@gmail.com>
2023-03-28 21:37:03 +1100

    loading indicator for file

--- zwf-web/src/components/Loading.js
+++ zwf-web/src/components/Loading.js
@@ -2,18 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
-const loadingIndicator = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
 export const Loading = props => {
-  const { loading, children } = props;
+  const { loading, size, children } = props;
 
-return <Spin spinning={loading} indicator={loadingIndicator} style={{width: '100%'}}>{children}</Spin>
+  return <Spin
+    spinning={loading}
+    indicator={<LoadingOutlined style={{ fontSize: size }} spin />}
+    style={{ width: '100%' }}
+  >
+    {children}
+  </Spin>
 }
 
 Loading.propTypes = {
   loading: PropTypes.bool.isRequired,
+  size: PropTypes.number,
 };
 
 Loading.defaultProps = {
-  loading: true
+  loading: true,
+  size: 24
 };
