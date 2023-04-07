dde86d10c4ba934c7a45fc0edb55490bdc0443c1
Jun Shao <mr.shaojun@gmail.com>
2023-04-07 14:13:11 +1000

    fix 403 error when setup org

--- zwf-app/src/utils/assertRole.ts
+++ zwf-app/src/utils/assertRole.ts
@@ -4,13 +4,13 @@ import { getOrgIdFromReq } from './getOrgIdFromReq';
 import { getRoleFromReq } from './getRoleFromReq';
 
 
-export function assertRole(req, roles) {
+export function assertRole(req, roles, allowImcompleteOrgAccess = false) {
   const role = getRoleFromReq(req);
   const orgId = getOrgIdFromReq(req);
 
   const hasOrg = !!orgId;
   const mustHaveOrgRole = role === Role.Admin || role === Role.Agent;
-  const matchesOrg = hasOrg === mustHaveOrgRole;
+  const matchesOrg = allowImcompleteOrgAccess ||  hasOrg === mustHaveOrgRole;
 
   assert(roles.includes(role) && matchesOrg, 403, `Invalid permission ('${role}' is to access '${roles.join()}')`);
 }
