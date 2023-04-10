625ad7567b34cb1a4e24bbf13d7b2844a1cf31eb
Jun Shao <mr.shaojun@gmail.com>
2023-04-11 09:44:13 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,13 +1,2 @@
 export const notes=`
-React warning fix
-wording fix
-bugfix
-email body
-fix sso welcome email
-clean up
-change bcc to info@
-change contact email address
-1 nature month to 30 days
-home page wording
-fix text
-clean up`
+google sso flow refine`
