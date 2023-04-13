c50b2f48adc22856a7ef65c1dadd0a2ee0d370ca
Jun Shao <mr.shaojun@gmail.com>
2023-04-14 00:29:30 +1000

    style refine

--- fln-web/craco.config.js
+++ fln-web/craco.config.js
@@ -16,7 +16,7 @@ const modifyVars = {
   // '@height-base': '32px',
   // '@height-lg': '40px',
   // '@height-sm': '24px',
-  '@border-radius-base': '0px',
+  '@border-radius-base': '4px',
   // '@font-family': "'klavika', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
   // '@table-padding-vertical': '4px',
   // '@table-padding-horizontal': '4px',
