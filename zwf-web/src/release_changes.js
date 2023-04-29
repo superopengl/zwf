5d2b65cedf7e89c38752a2753575380ffe97fc1f
Jun Shao <mr.shaojun@gmail.com>
2023-04-29 22:54:58 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,17 +1,6 @@
 export const notes=`
-no tu and pp on sider footer
-fix member select
-more client name card fix
-fix client name cards in recurring
-fix client name cards
-org client fix
-refine click to edit input
-change client alias
-name card with alias
-user tags moved to org client
-able to add client without email
-tag select refine
-db schema fix
-fix login error notification bug
-orgClient can be without userId
-typo fix`
+bugfix form field editor
+code clean up
+fix React warning
+fix incorrect zevent initiation
+no logout confirm`
