bb8e70c3a14ed422743c8221caf61906c65105ec
Jun Shao <mr.shaojun@gmail.com>
2023-04-30 21:15:48 +1000

    validate field name duplicate

---
+++ zwf-app/src/utils/validateFormFields.ts
@@ -0,0 +1,19 @@
+import { assert } from './assert';
+import _ from 'lodash';
+
+export function validateFormFields(fields: any) {
+  assert(fields?.length, 400, 'fields is empty.');
+
+  const set = new Set();
+  const dups = [];
+  for (const field of fields) {
+    const { name } = field;
+    if (set.has(name)) {
+      dups.push(name)
+    } else {
+      set.add(name);
+    }
+  }
+
+  assert(!dups.length, 400, `Field name ${dups.map(x => `"${x}"`).join(',')} are not unique.`);
+}
