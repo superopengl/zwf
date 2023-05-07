91414ae6ac5b3afd2f78f713edaf8a9c1eb7a2d0
Jun Shao <mr.shaojun@gmail.com>
2023-05-07 23:36:51 +1000

    remove est and due date

--- zwf-app/src/entity/Task.ts
+++ zwf-app/src/entity/Task.ts
@@ -30,15 +30,6 @@ export class Task {
   @Index()
   updatedAt: Date;
 
-  @Column({nullable: true})
-  estNumber: string;
-
-  @Column({nullable: true})
-  estUnit: 'hour' | 'day' | 'week';
-
-  @Column({nullable: true})
-  dueAt: Date;
-
   @Column()
   name: string;
 
