082194841a43fe7c8cc47a9e2a46905aef09ba0f
Jun Shao <mr.shaojun@gmail.com>
2023-05-11 07:34:33 +1000

    refine table

--- zdn-app/src/entity/Subscription.ts
+++ zdn-app/src/entity/Subscription.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, Index, OneToOne, JoinColumn, OneToMany, CreateDateColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Index, OneToOne, JoinColumn, OneToMany, CreateDateColumn, Unique } from 'typeorm';
 import { PaymentMethod } from '../types/PaymentMethod';
 import { SubscriptionStatus } from '../types/SubscriptionStatus';
 import { SubscriptionType } from '../types/SubscriptionType';
@@ -9,7 +9,9 @@ import { ColumnNumericTransformer } from '../utils/ColumnNumericTransformer';
 @Entity()
 @Index('idx_subscription_end', ['end'])
 @Index('idx_subscription_orgId_start_end', ['orgId', 'start', 'end'])
-@Index('idx_subscription_orgId_createdAt', ['orgId', 'createdAt'])
+@Index('idx_subscription_orgId_end', ['orgId', 'end'])
+@Index('idx_subscription_orgId_alive_unique', ['orgId'], { where: `status = '${SubscriptionStatus.Alive}'`, unique: true })
+@Index('idx_subscription_orgId_trial_unique', ['orgId'], { where: `type = '${SubscriptionType.Trial}'`, unique: true })
 export class Subscription {
   @PrimaryGeneratedColumn('uuid')
   id?: string;
@@ -35,10 +37,10 @@ export class Subscription {
   @Column('decimal', { transformer: new ColumnNumericTransformer() })
   totalPrice: number;
 
-  @Column()
+  @Column('date')
   start: Date;
 
-  @Column()
+  @Column('date')
   end: Date;
 
   @Column({ default: SubscriptionStatus.Provisioning })
