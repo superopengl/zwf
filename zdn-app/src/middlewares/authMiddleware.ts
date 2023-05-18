accfbde79f41890f5c4074c3fe93c3f0da6c53e0
Jun Shao <mr.shaojun@gmail.com>
2023-05-19 08:07:42 +1000

    refine auth middleware

--- zdn-app/src/middlewares/authMiddleware.ts
+++ zdn-app/src/middlewares/authMiddleware.ts
@@ -2,30 +2,46 @@
 import { getRepository } from 'typeorm';
 import { User } from '../entity/User';
 import { getNow } from '../utils/getNow';
+import { getUtcNow } from '../utils/getUtcNow';
 import { verifyJwtFromCookie, attachJwtCookie, clearJwtCookie } from '../utils/jwt';
+import * as moment from 'moment';
+import { getActiveUserByEmail } from '../utils/getActiveUserByEmail';
 
 export const authMiddleware = async (req, res, next) => {
+
   try {
-    const user = verifyJwtFromCookie(req);
+    let user = verifyJwtFromCookie(req);
+
     if (user) {
       // Logged in users
-      const { id } = user;
-      const repo = getRepository(User);
-      const existing = await repo.findOne(id);
-      if (!existing) {
-        clearJwtCookie(res);
-        res.sendStatus(401);
-        return;
+      const { expires } = user;
+      if (moment(expires).isBefore()) {
+        // JWT token expired. Needs to refresh
+        const existingUser = await getActiveUserByEmail(user.profile.email);
+        if (!existingUser) {
+          // User not existing anymore
+          clearJwtCookie(res);
+          res.sendStatus(401);
+          return;
+        }
+        // console.log('Renewed cookie for'.green, user.profile.email);
+
+        user = existingUser;
       }
-      repo.update(id, { lastNudgedAt: getNow() }).catch(() => { });
+      getRepository(User).update({ id: user.id }, { lastNudgedAt: getUtcNow() }).catch(() => { });
       req.user = Object.freeze(user);
-      attachJwtCookie(user as User, res);
+      attachJwtCookie(user, res);
     } else {
       // Guest user (hasn't logged in)
+      // req.user = null;
+      // clearJwtCookie(res);
     }
   } catch {
     clearJwtCookie(res);
   }
+
+  // console.log('Auth done'.green, req.method, req.url, req.user);
+
   next();
 };
 
