484e22fe76f35997dfdfaf6384f4e9bcb934a4a7
Jun Shao <mr.shaojun@gmail.com>
2023-05-28 19:38:11 +1000

    view refine

--- zwf-app/src/entity/views/TaskWatcherEmailNotificationInformation.ts
+++ zwf-app/src/entity/views/TaskWatcherEmailNotificationInformation.ts
@@ -1,31 +1,64 @@
-import { ViewEntity, DataSource, ViewColumn } from 'typeorm';
+import { ViewEntity, DataSource, ViewColumn, IsNull } from 'typeorm';
 import { UserInformation } from './UserInformation';
 import { TaskWatcherUiNotificationInformation } from './TaskWatcherUiNotificationInformation';
+import { ZeventDef } from '../ZeventDef';
+import { TaskWatcherEventAckInformation } from './TaskWatcherEventAckInformation';
 
 @ViewEntity({
   expression: (connection: DataSource) => connection
     .createQueryBuilder()
-    .from(TaskWatcherUiNotificationInformation, 'x')
-    .innerJoin(UserInformation, 'u', `u.id = x."userId"`)
-    .where(`x."unackDays" IN (1, 3, 7, 15, 30)`)
+    .from(q => q
+      .from(TaskWatcherEventAckInformation, 'x')
+      .innerJoin(UserInformation, 'u', `u.id = x."userId"`)
+      .innerJoin(ZeventDef, 'z', 'u.role = ANY(z."emailNotifyRoles") AND x.type = z.name')
+      .where({ ackAt: IsNull() })
+      .orderBy('x."createdAt"', 'ASC')
+      .select([
+        `x."taskId" as "taskId"`,
+        `x."taskName" as "taskName"`,
+        `x."userId" as "userId"`,
+        `x."createdAt" as "createdAt"`,
+        `x."type" as "type"`,
+        `u.email as email`,
+        `u."givenName" as "givenName"`,
+        `u."surname" as "surname"`,
+      ]), 'm')
+    .distinctOn([
+      '"taskId"',
+      '"taskName"',
+      '"userId"',
+      '"email"',
+      '"givenName"',
+      '"surname"',
+    ])
     .select([
-      `x."taskId" as "taskId"`,
-      `x."taskName" as "taskName"`,
-      `x."userId" as "userId"`,
-      `u."email" as "email"`,
-      `u."givenName" as "givenName"`,
-      `u."surname" as "surname"`,
-    ]),
-  dependsOn: [TaskWatcherUiNotificationInformation]
-}) export class TaskWatcherEmailNotificationInformation {
-  @ViewColumn()
-  userId: string;
+      '"taskId"',
+      '"taskName"',
+      '"userId"',
+      '"email"',
+      '"givenName"',
+      '"surname"',
+      `extract(day from NOW() - "createdAt") AS "unackDays"`,
+    ])
+    .orderBy('"taskId"')
+    .addOrderBy('"taskName"')
+    .addOrderBy('"userId"')
+    .addOrderBy('"email"')
+    .addOrderBy('"givenName"')
+    .addOrderBy('"surname"')
+    .addOrderBy('"createdAt"', 'ASC')
+  ,
 
+  dependsOn: [TaskWatcherEventAckInformation]
+}) export class TaskWatcherEmailNotificationInformation {
   @ViewColumn()
   taskId: string;
 
   @ViewColumn()
   taskName: string;
+
+  @ViewColumn()
+  userId: string;
 
   @ViewColumn()
   email: string;
@@ -35,6 +68,9 @@ import { TaskWatcherUiNotificationInformation } from './TaskWatcherUiNotificatio
 
   @ViewColumn()
   surname: string;
+
+  @ViewColumn()
+  unackDays: number;
 }
 
 
