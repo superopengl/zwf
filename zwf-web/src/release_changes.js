42c5f8ca4b51404c05a2f9d53ed08a05420d0e6f
Jun Shao <mr.shaojun@gmail.com>
2023-06-19 21:37:10 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,8 +1,2 @@
 export const notes=`
-old pdf preview for release
-add builtin demplate/template upon org creation
-view bug fix
-server side rendering for demplate preview
-demplate preview API
-add tinymce exports plugin
-using html2pdf to render PDF in frontend`
+bugfix`
