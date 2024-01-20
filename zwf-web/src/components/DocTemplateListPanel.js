6113d8a6c4c0fb7bae048234cdcf24d6bb368cd5
Jun Shao <mr.shaojun@gmail.com>
2024-01-20 18:27:31 +1100

    prefill doc template on create task

--- zwf-web/src/components/DocTemplateListPanel.js
+++ zwf-web/src/components/DocTemplateListPanel.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { List, Typography } from 'antd';
+import { Badge, List, Typography, Tooltip } from 'antd';
 import { DocTemplateIcon } from 'components/entityIcon';
 import styled from 'styled-components';
 import { getDocTemplate$ } from 'services/docTemplateService';
 import { showDocTemplatePreviewModal } from './showDocTemplatePreviewModal';
+import { VarTag } from './VarTag';
+import { ExclamationCircleFilled } from '@ant-design/icons';
 
 const { Text } = Typography;
 
@@ -24,16 +26,12 @@ padding-right: 12px !important;
 `;
 
 export const DocTemplateListPanel = (props) => {
-  const { value: docs, allowTest, varBag, showWarning, ...otherProps } = props;
+  const { value: docs, allowTest, varBag, showWarning, renderVariable, ...otherProps } = props;
 
   const handlePreviewDocTemplate = docId => {
     getDocTemplate$(docId).subscribe(docTemplate => {
-      showDocTemplatePreviewModal(docTemplate, { allowTest, varBag, showWarning });
+      showDocTemplatePreviewModal(docTemplate, { allowTest, varBag });
     })
-  }
-
-  const getDocMissingVars = (doc) => {
-    
   }
 
   return docs?.length > 0 && <List
@@ -42,10 +40,18 @@ export const DocTemplateListPanel = (props) => {
     {...otherProps}
     rowKey="id"
     dataSource={docs}
-    renderItem={doc => <DocListItem onClick={() => handlePreviewDocTemplate(doc.id)}>
-      <div><DocTemplateIcon /><Text>{doc.name}</Text></div>
-     {showWarning && <div>{getDocMissingVars(doc)}</div>}
-    </DocListItem>}
+    renderItem={doc => {
+      const missingVarComps = (doc.variables ?? []).filter(v => varBag[v] === undefined || varBag[v] === '').map(v => renderVariable(v));
+      return <DocListItem onClick={() => handlePreviewDocTemplate(doc.id)}>
+        <div><DocTemplateIcon /><Text>{doc.name}</Text>
+          {showWarning && missingVarComps.length > 0 && <>
+            <Tooltip title={<>Fields {missingVarComps} are required to be input to generate the doc.</>}>
+              <ExclamationCircleFilled style={{ color: '#cf1322', marginLeft: 4, fontSize: 18 }} />
+            </Tooltip>
+          </>}
+        </div>
+      </DocListItem>
+    }}
   />
 };
 
@@ -54,6 +60,7 @@ DocTemplateListPanel.propTypes = {
   allowTest: PropTypes.bool,
   showWarning: PropTypes.bool,
   varBag: PropTypes.object,
+  renderVariable: PropTypes.func,
 };
 
 DocTemplateListPanel.defaultProps = {
@@ -61,5 +68,6 @@ DocTemplateListPanel.defaultProps = {
   allowTest: false,
   showWarning: false,
   varBag: {},
+  renderVariable: (varName) => <VarTag>{varName}</VarTag>
 };
 
