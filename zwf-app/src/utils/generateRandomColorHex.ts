5ade9d7632de1711aba129aebf647b9e15a3c83f
Jun Shao <mr.shaojun@gmail.com>
2024-01-20 15:59:01 +1100

    refine random color

--- zwf-app/src/utils/generateRandomColorHex.ts
+++ zwf-app/src/utils/generateRandomColorHex.ts
@@ -3,8 +3,8 @@ import * as uniqolor from 'uniqolor';
 export function generateRandomColorHex(): string {
   const { color } = uniqolor.random({
     format: 'hex',
-    saturation: [0, 100],
-    lightness: [0, 49],
+    saturation: [50, 100],
+    lightness: [60, 90],
     differencePoint: 160
   });
   return color;
