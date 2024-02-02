d07d72fa69cd62721f5c0a6606fba9a625d6c135
Jun Shao <mr.shaojun@gmail.com>
2024-02-02 23:15:51 +1100

    client task list ui refine

--- zwf-web/src/pages/ClientTask/ClientTaskListPage.js
+++ zwf-web/src/pages/ClientTask/ClientTaskListPage.js
@@ -1,5 +1,5 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Layout, Row, Col, Space, Spin, Typography, List } from 'antd';
+import { Button, Layout, Row, Col, Space, Spin, Typography, List, Tabs, Grid, Alert } from 'antd';
 import Text from 'antd/lib/typography/Text';
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
@@ -10,18 +10,61 @@ import { TaskDraggableCard } from '../../components/TaskDraggableCard';
 import { Loading } from 'components/Loading';
 import { TaskClientCard } from 'components/TaskClientCard';
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
+const { useBreakpoint } = Grid;
+
 
 const LayoutStyled = styled.div`
   margin: 0 auto 0 auto;
   // background-color: #ffffff;
   height: 100%;
+  max-width: 1500px;
 `;
+
+const TAB_DEFS = [
+  {
+    label: 'Action required',
+    default: true,
+    description: 'These tasks requires your actions by either filling the form, upload required files, or reply messages from the agents',
+    filter: item => {
+      return true;
+    }
+  },
+  {
+    label: 'In progress',
+    description: 'These are the tasks that are being proceeded by your agents. No immidiate action is required from your side at the moment.',
+    filter: item => {
+      return true;
+    }
+  },
+  {
+    label: 'Recently completed (30 days)',
+    description: 'The tasks that have been completed in the past 30 days.',
+    filter: item => {
+      return true;
+    }
+  },
+  {
+    label: 'Past tasks',
+    description: 'All the past tasks that are either completed or archived.',
+    filter: item => {
+      return true;
+    }
+  },
+  {
+    label: 'All tasks',
+    description: 'All the tasks.',
+    filter: item => {
+      return true;
+    }
+  },
+];
 
 
 export const ClientTaskListPage = withRouter(() => {
   const [loading, setLoading] = React.useState(true);
   const [list, setList] = React.useState([]);
+  const screens = useBreakpoint();
 
   React.useEffect(() => {
     const sub$ = listTask$()
@@ -35,22 +78,29 @@ export const ClientTaskListPage = withRouter(() => {
 
   return (
     <LayoutStyled>
-      <List
-        loading={loading}
-        dataSource={list}
-        grid={{
-          gutter: 24,
-          xs: 1,
-          sm: 1,
-          md: 1,
-          lg: 1,
-          xl: 2,
-          xxl: 3,
-        }}
-        renderItem={item => <List.Item>
-          <TaskClientCard task={item} />
-        </List.Item>}
-      />
+      <Tabs tabPosition={screens.md ? 'left' : 'top'}
+       type="line"
+        defaultActiveKey={TAB_DEFS.find(x => x.default)?.label}>
+        {TAB_DEFS.map(tab => <Tabs.TabPane tab={tab.label} key={tab.label}>
+          <Alert description={tab.description} type="info" showIcon style={{marginBottom: 20}}/>
+          <List
+            loading={loading}
+            dataSource={list.filter(tab.filter)}
+            grid={{
+              gutter: 24,
+              xs: 1,
+              sm: 1,
+              md: 1,
+              lg: 1,
+              xl: 2,
+              xxl: 2,
+            }}
+            renderItem={item => <List.Item>
+              <TaskClientCard task={item} />
+            </List.Item>}
+          />
+        </Tabs.TabPane>)}
+      </Tabs>
     </LayoutStyled>
   )
 })
