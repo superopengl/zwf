247f3e07e81743f097980d6f59ccb8c9801f7d17
Jun Shao <mr.shaojun@gmail.com>
2024-02-14 08:05:28 +1100

    refine gen doc and attachment panel

--- zwf-web/src/components/TaskAttachmentPanel.js
+++ zwf-web/src/components/TaskAttachmentPanel.js
@@ -1,37 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Space, List, Typography, Badge, Row, Tag, Upload, Button, Modal, Checkbox, Table, Tooltip } from 'antd';
+import { List, Typography, Upload, Modal } from 'antd';
 import { DocTemplateIcon } from 'components/entityIcon';
 import styled from 'styled-components';
-import { getDocTemplate$ } from 'services/docTemplateService';
-import { showDocTemplatePreviewModal } from './showDocTemplatePreviewModal';
 import { VarTag } from './VarTag';
-import Icon, { DeleteOutlined, ExclamationCircleFilled, ExclamationCircleOutlined, EyeOutlined, PlusOutlined } from '@ant-design/icons';
-import { getFileUrl, getPublicFileUrl, openFile } from 'services/fileService';
 import { API_BASE_URL } from 'services/http';
-import { TimeAgo } from './TimeAgo';
-import { createOrphanTaskDocFromDocTemplate$, createOrphanTaskDocFromUploadedFile$, genDoc$, getTaskDocDownloadUrl, listTaskDocs$, signTaskDoc$, toggleTaskDocsOfficialOnly$, toggleTaskDocsRequiresSign$ } from "services/taskDocService";
+import { createOrphanTaskDocFromDocTemplate$, createOrphanTaskDocFromUploadedFile$, listTaskDocs$ } from "services/taskDocService";
 import { finalize } from 'rxjs/operators';
 import { notify } from 'util/notify';
-import { FileIcon } from './FileIcon';
-import { GlobalContext } from 'contexts/GlobalContext';
-import { FaFileSignature, FaSignature } from 'react-icons/fa';
-import { ConfirmDeleteButton } from './ConfirmDeleteButton';
 import { Subscription } from 'rxjs';
 import { AddNewTaskDocItem } from './AddNewTaskDocItem';
 import { AddFromDocTemplateItem } from './AddFromDocTemplateItem';
 import { TaskDocItem } from './TaskDocItem';
-import { showSignTaskDocModal } from './showSignTaskDocModal';
-import TaskTemplateSelect from './TaskTemplateSelect';
 import DocTemplateSelect from './DocTemplateSelect';
-import { BsFillPatchCheckFill, BsPatchCheck } from 'react-icons/bs';
-import { Logo } from './Logo';
-import { MdBrightnessAuto } from 'react-icons/md';
 
-const { Text, Link: TextLink, Paragraph } = Typography;
+const { Paragraph } = Typography;
 
 const Container = styled.div`
-
 .ant-upload-btn {
   margin: 0;
   padding: 0 !important;
@@ -52,9 +37,21 @@ const Container = styled.div`
     border: 0;
   }
 }
-
 `;
 
+const StyledList = styled(List)`
+.ant-list-item-meta-title {
+  margin-bottom: 0;
+
+  .ant-typography {
+    font-size: 14px;
+  }
+}
+
+.ant-list-item:hover {
+  background-color: rgb(250, 250, 250);
+}
+`;
 
 
 const LAST_ADD_DOC_BUTTON_ITEM = {
@@ -72,16 +69,11 @@ const LAST_DOC_TEMPLATEBUTTON_ITEM = {
 const EXTRA_ITEMS = [LAST_DOC_TEMPLATEBUTTON_ITEM, LAST_ADD_DOC_BUTTON_ITEM];
 
 export const TaskAttachmentPanel = (props) => {
-  const { value: taskDocIds, disabled, varBag, onChange, showWarning, renderVariable, mode } = props;
+  const { value: taskDocIds, disabled, varBag, onChange, showWarning, mode } = props;
 
   const [list, setList] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const [docTemplateModalVisible, setDocTemplateModalVisible] = React.useState(false);
-  const context = React.useContext(GlobalContext);
-  const { user, role } = context;
-
-  const isClient = role === 'client';
-  const isAgent = role === 'agent' || role === 'admin';
 
   React.useEffect(() => {
     const sub$ = reload$(true);
@@ -126,25 +118,6 @@ export const TaskAttachmentPanel = (props) => {
     setLoading(uploading);
   };
 
-  const handleDeleteDoc = (item, e) => {
-    e.stopPropagation();
-    const taskDocId = item.id;
-    setList(list => {
-      const newList = list.filter(x => x.id !== taskDocId);
-      onChange(newList.map(x => x.id));
-      return newList;
-    })
-  }
-
-  const handleGenDoc = (item, e) => {
-    e.stopPropagation();
-    const taskDocId = item.id;
-    setLoading(true);
-    genDoc$(taskDocId).subscribe(() => {
-      reload$(true)
-    });
-  }
-
   const beforeUpload = (file) => {
     const isLt20M = file.size / 1024 / 1024 < 20;
     if (!isLt20M) {
@@ -153,108 +126,20 @@ export const TaskAttachmentPanel = (props) => {
     return isLt20M;
   }
 
-  const canDelete = (taskDoc) => {
-    switch (taskDoc.type) {
-      case 'client':
-        return role === 'client' && user.id === taskDoc.createdBy && !taskDoc.signedAt
-      case 'auto':
-      case 'agent':
-        return (role === 'admin' || role === 'agent') && !taskDoc.signedAt;
-    }
-    return false;
-  }
-
-  const canToggleOfficalOnly = (taskDoc) => {
-    return !taskDoc.isExtra && isAgent && taskDoc.type !== 'client' && !taskDoc.signedAt
-  }
-
-  const canRequestClientSign = (taskDoc) => {
-    return !taskDoc.isExtra && isAgent && taskDoc.type !== 'client' && taskDoc.fileId && !taskDoc.signedAt
-  }
-
-  const canClientSign = (taskDoc) => {
-    return !taskDoc.isExtra && isClient && taskDoc.requiresSign && !taskDoc.signedAt
-  }
-
-  const canGenDoc = (taskDoc) => {
-    return !taskDoc.isExtra && !isClient && taskDoc.docTemplateId && !taskDoc.fileId
-  }
-
-  const handleToggleOfficialOnly = (taskDoc, e) => {
-    e.stopPropagation();
-    const checked = e.target.checked;
-    setLoading(true);
-    toggleTaskDocsOfficialOnly$(taskDoc.id, checked).subscribe(() => {
-      taskDoc.officialOnly = checked;
-      reload$()
-    })
-  }
-
-  const handleToggleRequireSign = (taskDoc, e) => {
-    e.stopPropagation();
-    const checked = e.target.checked;
-    setLoading(true);
-    toggleTaskDocsRequiresSign$(taskDoc.id, checked).subscribe(() => {
-      taskDoc.requiresSign = checked;
-      reload$(true)
-    })
-  }
-
-  const handleSignTaskDoc = (taskDoc, e) => {
-    e.stopPropagation();
-    // setLoading(true);
-    // signTaskDoc$(taskDoc.id).subscribe(() => {
-    //   reload$(true);
-    // })
-    showSignTaskDocModal(taskDoc, {
-      onOk: () => {
-        reload$(true);
-      }
-    })
-  }
-
   const listDataSource = React.useMemo(() => {
     return disabled ? list : [...list, ...EXTRA_ITEMS];
   }, [list]);
 
-  const columns = React.useMemo(() => [
-    {
-      render: (value, item) => <big>
-        {item.isAddButton ? <AddNewTaskDocItem /> :
-          item.isAddDocTemplateButton ? <AddFromDocTemplateItem /> :
-            <TaskDocItem 
-            taskDoc={item} 
-            showCreatedAt={true} 
-            varBag={varBag}
-            // iconOverlay={item.type === 'auto' ? <Icon component={() => <MdBrightnessAuto/>} style={{color: '#37AFD2', fontSize: 20}} /> : null}
-            description={item.type === 'auto' ? <Text type="danger">Automatically generated doc, pending fields</Text> : null} 
-            />}
-      </big>
-    },
-    isClient ? null : {
-      title: 'Require sign',
-      // width: 20,
-      align: 'center',
-      render: (value, item) => item.signedAt ? <TimeAgo value={item.signedAt} prefix="Signed:" accurate={false} showTime={false} />
-        : canRequestClientSign(item) ? <Checkbox key="official" checked={item.requiresSign} onClick={(e) => handleToggleRequireSign(item, e)} />
-          : null
-    },
-    isClient ? null : {
-      title: 'Hide from client?',
-      width: 20,
-      align: 'center',
-      render: (value, item) => item.isExtra ? null : <Checkbox key="official" checked={item.officialOnly} onClick={(e) => handleToggleOfficialOnly(item, e)} disabled={!canToggleOfficalOnly(item)} />
-    },
-    {
-      // width: 20,
-      align: 'right',
-      render: (value, item) => <>
-        {canGenDoc(item) && <Tooltip title="generate doc"><Button type="link" icon={<Icon component={() => <BsPatchCheck />} />} onClick={(e) => handleGenDoc(item, e)} ></Button></Tooltip>}
-        {canDelete(item) && <ConfirmDeleteButton danger type="text" icon={<DeleteOutlined />} onOk={(e) => handleDeleteDoc(item, e)} ></ConfirmDeleteButton>}
-        {canClientSign(item) && <Button type="link" icon={<Icon component={() => <FaFileSignature />} />} onClick={(e) => handleSignTaskDoc(item, e)}>sign</Button>}
-      </>
-    }
-  ].filter(x => x), [varBag]);
+  const handleDeleteDoc = (item) => {
+    const taskDocId = item.id;
+    const newList = list.filter(x => x.id !== taskDocId);
+    setList(newList)
+    onChange(newList.map(x => x.id));
+  }
+
+  const handleTaskDocChange = () => {
+    onChange(list.map(x => x.id))
+  }
 
   const handleAddDocTemplate = (docTemplateId) => {
     createOrphanTaskDocFromDocTemplate$(docTemplateId).subscribe(taskDoc => {
@@ -277,38 +162,26 @@ export const TaskAttachmentPanel = (props) => {
       itemRender={() => null}
       onChange={handleUploadFile}
       disabled={disabled}
+      style={{backgroundColor: 'transparent'}}
     >
-      <Table
+      <StyledList
         dataSource={listDataSource}
-        columns={columns}
         loading={loading}
-        showHeader={isAgent}
-        // size="small"
-        locale={{
-          emptyText: 'No file uploaded. You can upload files when the agent requires.'
-        }}
-        pagination={false}
-        rowKey={item => item.id}
-        onRow={(item) => {
-          return {
-            onClick: e => {
-              if (!item.isAddButton) {
-                e.stopPropagation();
-              }
-              if (item.isAddDocTemplateButton) {
-                setDocTemplateModalVisible(true);
-              }
-              return null;
-            },
-            onDoubleClick: e => item.isAddButton ? null : e.stopPropagation(),
-          }
-        }}
-        onHeaderRow={() => {
-          return {
-            onClick: e => e.stopPropagation(),
-            onDoubleClick: e => e.stopPropagation(),
-          }
-        }}
+        itemLayout="vertical"
+        size="small"
+        renderItem={item => item.isAddButton ? <AddNewTaskDocItem key="new-file" /> :
+          item.isAddDocTemplateButton ? <AddFromDocTemplateItem key="new-doc-template" onClick={() => setDocTemplateModalVisible(true)} /> :
+            <TaskDocItem
+              key={item.id}
+              taskDoc={item}
+              showCreatedAt={true}
+              varBag={varBag}
+              onChange={() => handleTaskDocChange(item)}
+              onDelete={() => handleDeleteDoc(item)}
+              onLoading={setLoading}
+            // iconOverlay={item.type === 'auto' ? <Icon component={() => <MdBrightnessAuto/>} style={{color: '#37AFD2', fontSize: 20}} /> : null}
+            />
+        }
       />
     </Upload.Dragger>
     <Modal
--- zwf-web/src/components/TaskDocItem.js
+++ zwf-web/src/components/TaskDocItem.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import PropTypes from 'prop-types';
-import { Typography, Spin, Space, Badge, Tooltip, Tag } from 'antd';
-import { getFileMeta } from 'services/fileService';
+import { Typography, Space, List, Tooltip, Tag, Checkbox, Button } from 'antd';
 import { FileIcon } from './FileIcon';
-import { from, Subscription } from 'rxjs';
-import { Loading } from './Loading';
 import { TimeAgo } from './TimeAgo';
-import { getTaskDocDownloadUrl } from 'services/taskDocService';
 import { GlobalContext } from 'contexts/GlobalContext';
 import { CheckOutlined } from '@ant-design/icons'
 import _ from 'lodash';
+import Icon, { DeleteOutlined } from '@ant-design/icons';
+import { BsPatchCheck } from 'react-icons/bs';
+import { showSignTaskDocModal } from './showSignTaskDocModal';
+import { FaFileSignature } from 'react-icons/fa';
+import { genDoc$, getTaskDocDownloadUrl, toggleTaskDocsOfficialOnly$, toggleTaskDocsRequiresSign$ } from "services/taskDocService";
+import { ConfirmDeleteButton } from './ConfirmDeleteButton';
+import { finalize } from 'rxjs/operators';
 
 const { Link, Text } = Typography;
+
+const StyledActionButtons = styled(Space)`
+gap: 0px !important;
+
+.ant-checkbox-wrapper {
+  margin: 5px 0;
+}
+
+button {
+  text-align: left;
+  padding-left: 0;
+}
+`;
 
 const getAutoDocTag = (taskDoc, role) => {
   if (role === 'client' || taskDoc.type !== 'auto') {
@@ -35,7 +53,7 @@ const getAutoDocTag = (taskDoc, role) => {
 const getMissingVarWarningMessage = (variables, varBag) => {
   const missingVars = [];
   // debugger;
-  for (const varName of variables) {
+  for (const varName of (variables || [])) {
     const def = varBag[varName];
     if (!def) {
       // Not found in varBag
@@ -48,30 +66,122 @@ const getMissingVarWarningMessage = (variables, varBag) => {
   return missingVars.length ? missingVars : null;
 }
 
-export const TaskDocItem = props => {
-  const { taskDoc, showIcon, style, showCreatedAt, strong, description, align, iconOverlay, varBag, fields } = props;
+export const TaskDocItem = React.memo(props => {
+  const { taskDoc, showIcon, style, showCreatedAt, strong, onChange, onDelete, iconOverlay, varBag, onLoading } = props;
   const context = React.useContext(GlobalContext);
+  const { user, role } = context;
+
+  const isClient = role === 'client';
+  const isAgent = role === 'agent' || role === 'admin';
 
   const missingVars = React.useMemo(() => {
     return getMissingVarWarningMessage(taskDoc.variables, varBag)
   }, [taskDoc, varBag]);
 
-  return <Space size="small" align={align} style={{ width: '100%', lineHeight: 1.4, ...style }}>
-    {showIcon && <div style={{ position: 'relative' }}>
-      <FileIcon name={taskDoc.name} />
-      {iconOverlay && <div
-        style={{ position: 'absolute', right: -2, top: -2 }}
-      >{iconOverlay}</div>}
-    </div>}
-    <div>
-      <Link href={getTaskDocDownloadUrl(taskDoc.id)} target="_blank" strong={strong}>
+  const canDelete = (taskDoc) => {
+    switch (taskDoc.type) {
+      case 'client':
+        return role === 'client' && user.id === taskDoc.createdBy && !taskDoc.signedAt
+      case 'auto':
+      case 'agent':
+        return (role === 'admin' || role === 'agent') && !taskDoc.signedAt;
+    }
+    return false;
+  }
+
+  const canToggleOfficalOnly = (taskDoc) => {
+    return isAgent && taskDoc.type !== 'client' && !taskDoc.signedAt
+  }
+
+  const canRequestClientSign = (taskDoc) => {
+    return isAgent && taskDoc.type !== 'client' && taskDoc.fileId && !taskDoc.signedAt
+  }
+
+  const canClientSign = (taskDoc) => {
+    return isClient && taskDoc.requiresSign && !taskDoc.signedAt
+  }
+
+  const canGenDoc = (taskDoc) => {
+    return !isClient && taskDoc.docTemplateId && !taskDoc.fileId
+  }
+
+  const handleToggleOfficialOnly = (taskDoc, e) => {
+    e.stopPropagation();
+    const checked = e.target.checked;
+    toggleTaskDocsOfficialOnly$(taskDoc.id, checked).subscribe(() => {
+      taskDoc.officialOnly = checked;
+      onChange()
+    })
+  }
+
+  const handleToggleRequireSign = (taskDoc, e) => {
+    e.stopPropagation();
+    const checked = e.target.checked;
+    onLoading(true)
+    toggleTaskDocsRequiresSign$(taskDoc.id, checked).pipe(
+      finalize(() => onLoading(false))
+    ).subscribe(() => {
+      onChange()
+    })
+  }
+
+  const handleSignTaskDoc = (taskDoc, e) => {
+    e.stopPropagation();
+    onLoading(true)
+    showSignTaskDocModal(taskDoc, {
+      onOk: () => {
+        onChange();
+        onLoading(false)
+      }
+    })
+  }
+
+  const handleDeleteDoc = (item, e) => {
+    e.stopPropagation();
+    onDelete();
+  }
+
+  const handleGenDoc = (item, e) => {
+    e.stopPropagation();
+    const taskDocId = item.id;
+    onLoading(true)
+    genDoc$(taskDocId).pipe(
+      finalize(() => onLoading(false))
+    ).subscribe(() => {
+      onChange();
+    });
+  }
+
+  return <List.Item onClick={e => {
+    e.stopPropagation();
+  }}
+    extra={<>
+      {taskDoc.signedAt ? <TimeAgo value={taskDoc.signedAt} prefix="Signed:" accurate={false} showTime={false} /> : <StyledActionButtons direction="vertical">
+        {canRequestClientSign(taskDoc) && <Checkbox key="official" checked={taskDoc.requiresSign} onClick={(e) => handleToggleRequireSign(taskDoc, e)} ><Link>Need client sign</Link></Checkbox>}
+        <Checkbox key="official" checked={taskDoc.officialOnly} onClick={(e) => handleToggleOfficialOnly(taskDoc, e)} disabled={!canToggleOfficalOnly(taskDoc)} ><Link>Hide from client</Link></Checkbox>
+        {canGenDoc(taskDoc) && <Button type="link" block icon={<Icon component={() => <BsPatchCheck />} />} onClick={(e) => handleGenDoc(taskDoc, e)} >Generate doc</Button>}
+        {canDelete(taskDoc) && <ConfirmDeleteButton danger type="text" icon={<DeleteOutlined />} onOk={(e) => handleDeleteDoc(taskDoc, e)} >Delete</ConfirmDeleteButton>}
+        {canClientSign(taskDoc) && <Button type="link" icon={<Icon component={() => <FaFileSignature />} />} onClick={(e) => handleSignTaskDoc(taskDoc, e)}>Sign</Button>}
+      </StyledActionButtons>}
+    </>}
+  >
+    <List.Item.Meta
+      avatar={<div style={{ position: 'relative' }}>
+        <FileIcon name={taskDoc.name} />
+        {iconOverlay && <div
+          style={{ position: 'absolute', right: -2, top: -2 }}
+        >{iconOverlay}</div>}
+      </div>}
+      title={<Link href={getTaskDocDownloadUrl(taskDoc.id)} target="_blank">
         {taskDoc.name} {getAutoDocTag(taskDoc, context.role)}
-      </Link>
-      {showCreatedAt && taskDoc.createdAt && <div><small><TimeAgo value={taskDoc.createdAt} prefix="Created:" direction="horizontal" /></small></div>}
-      {missingVars && <div style={{marginTop: 4}}><small>{missingVars.map(x => <div><Text type="danger">{x}</Text></div>)}</small></div>}
-    </div>
-  </Space>
-};
+      </Link>}
+      description={<>
+        {showCreatedAt && taskDoc.createdAt && <div><TimeAgo value={taskDoc.createdAt} prefix="Created:" direction="horizontal" /></div>}
+        {missingVars && <div style={{ marginTop: 4 }}>{missingVars.map(x => <div><Text type="danger">{x}</Text></div>)}</div>}
+      </>}
+    />
+  </List.Item>
+});
 
 TaskDocItem.propTypes = {
   taskDoc: PropTypes.shape({
@@ -81,18 +191,19 @@ TaskDocItem.propTypes = {
   }).isRequired,
   showIcon: PropTypes.bool,
   showCreatedAt: PropTypes.bool,
-  strong: PropTypes.bool,
-  description: PropTypes.object,
+  onChange: PropTypes.func,
+  onDelete: PropTypes.func,
+  onLoading: PropTypes.func,
   iconOverlay: PropTypes.object,
-  align: PropTypes.oneOf(['start', 'center']),
   varBag: PropTypes.object,
 };
 
 TaskDocItem.defaultProps = {
   showIcon: true,
   showCreatedAt: false,
-  strong: false,
-  align: 'start',
   varBag: {},
+  onChange: () => { },
+  onDelete: () => { },
+  onLoading: () => { },
 }
 
