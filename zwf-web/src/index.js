dd03f3618a085aa0a24ad6086b48a0fdb98de5ee
Jun Shao <mr.shaojun@gmail.com>
2024-02-21 17:59:27 +1100

    disable service worker and notification

--- zwf-web/src/index.js
+++ zwf-web/src/index.js
@@ -18,19 +18,19 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals(console.log);
 
-Notification.requestPermission().then((result) => {
-  if (result === 'granted') {
-    console.log('Notification and Push are granted');
-  }
-});
+// Notification.requestPermission().then((result) => {
+//   if (result === 'granted') {
+//     console.log('Notification and Push are granted');
+//   }
+// });
 
 // function randomNotification() {
 //   const notifTitle = 'ZeeWorkflow';
