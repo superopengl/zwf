e01efc74f959a0938067a854f862dfadd8a46a16
Jun Shao <mr.shaojun@gmail.com>
2024-02-29 21:07:53 +1100

    auto gen release note

---
+++ zwf-web/src/release_changes.js
@@ -0,0 +1,13 @@
+export const tag='Release-20220323-1245'
+export const notes=`
+don't talk with oneself
+homepage hooks
+fix unique email address
+code clean up
+refine contact
+supporter view fix
+support chat message count fix
+ support refine
+ui refine
+chat ux refine
+contact bugfix`
