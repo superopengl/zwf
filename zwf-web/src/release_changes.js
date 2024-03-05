3403462f93d3b5c86fdbb037eecc4a02fe15b8d8
Jun Shao <mr.shaojun@gmail.com>
2024-03-05 17:32:49 +1100

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,13 +1,3 @@
 export const notes=`
-lighthouse refine
-ui refine
-title
-typo
-ui refine
-page document title
-resource page UI refine
-resource pages publish/edit
-page edit list page
-resource pages
-resource api
-release fix`
+saving affix
+resource page picture`
