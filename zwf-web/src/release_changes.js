98e1d6af6c1e884a298a535fee423a7f168b104a
Jun Shao <mr.shaojun@gmail.com>
2024-05-05 05:35:15 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,6 +1,111 @@
 export const notes=`
-update release notes
-misc fix
+recurring implementation
+add view
+recurring draft
+clean up portfolio
+refine
+clean up
+new release notification ui refine
+emailer for require action email
+notify task
+fix warning
+improve modals
+clean up emails
+ui bugfix
 ui refine
+code clean up
+control disable styling
+complete task action
+doc template refine
+fix missing db entity
+open support for clients
+able to sign auto gen doc
+bugfix sign doc API
+file icon improvement
+clean up
+refine autodoc generation
+fix view
+display generated doc
+set autodoc page margin
 ui refine
-hide menu procedure`
+fix auto doc icon
+esign task files
+clean up
+misc
+refine sign doc ux and workflow
+bugfix TaskFileUploader
+refine upload control
+add utility TaskFileInformation view
+icon fix
+fix loading on subscription page
+add varbag and varbag hash to auto docs
+clean up
+add comment
+TaskFileInformation view
+refactor doc upload
+gen auto doc
+refine
+ui refine
+remove variable concept
+wording
+skip validation when edit task fields
+bugfix
+refine
+fix delete field task doc
+bugfix
+download field doc
+edit picture when uploading avatar
+refine upload
+add missing entity
+server-end delete orphan task fields
+improve view entity dependency
+fix nested form warning
+able to delete field on edit task fields
+comment
+downgrade to react 17
+update deps
+fix dnd
+bugfix
+fix default field
+more fix
+move fix
+dnd draft
+use react-dnd on task board
+add react-dnd
+code cleanup
+more fix
+upgrade params
+more fix for upgrading
+upgrade to react-router-dom 6
+refine
+fix field order
+typeorm fix
+upgrade to typeorm@0.3.6
+var rename
+bugfix
+fix edit fields on task
+debounced save fields
+save fields changes on TaskFields
+fix options issue
+bugfix: task status update
+add missing officialOnly property
+rectangular structure of task, field, doc, file
+clean up
+able to edit field on task
+remove number input precision
+docTemplatePage able to go back
+ui refine
+fix deep route
+task template editor refine
+task template builder refine
+task template editor refine
+improve options validation
+number control ui refine
+fix date control value issue
+fix date-picker moment <=> date issue
+fix navigation
+task template form validation
+ui refine
+highlighted search text
+user support page search
+menu refine`
