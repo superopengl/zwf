639936ac75062cb9a779d5d0fc80d7c26645b288
Jun Shao <mr.shaojun@gmail.com>
2024-05-05 07:40:35 +1000

    set puppeteer --no-sandbox

--- zwf-app/src/utils/generatePdfBufferFromHtml.ts
+++ zwf-app/src/utils/generatePdfBufferFromHtml.ts
@@ -2,6 +2,7 @@ import * as puppeteer from 'puppeteer';
 
 export async function generatePdfBufferFromHtml(html) {
   const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     headless: true
   })
   const page = await browser.newPage()
