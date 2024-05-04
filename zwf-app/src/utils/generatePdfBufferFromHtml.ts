9c1cedd36e6c106e4859c3f6e8223878e1421211
Jun Shao <mr.shaojun@gmail.com>
2024-05-05 07:11:16 +1000

    use puppeteer to replace html-pdf

--- zwf-app/src/utils/generatePdfBufferFromHtml.ts
+++ zwf-app/src/utils/generatePdfBufferFromHtml.ts
@@ -1,12 +1,22 @@
-import * as pdf from 'html-pdf';
+import * as puppeteer from 'puppeteer';
 
-
-export async function generatePdfBufferFromHtml(html, options) {
-  return new Promise<any>((res, rej) => {
-    pdf.create(html, options).toBuffer((err, buffer) => {
-      if (err)
-        return rej(err);
-      res(buffer);
-    });
-  });
+export async function generatePdfBufferFromHtml(html) {
+  const browser = await puppeteer.launch({
+    headless: true
+  })
+  const page = await browser.newPage()
+  await page.setContent(html, {
+    waitUntil: 'domcontentloaded'
+  })
+  const pdfBuffer = await page.pdf({
+    format: 'A4',
+    margin: {
+      top: '0.5in',
+      right: '0.5in',
+      bottom: '0.5in',
+      left: '0.5in',
+    }
+  })
+  await browser.close()
+  return pdfBuffer;
 }
