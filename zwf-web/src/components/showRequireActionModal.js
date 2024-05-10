8f5b399c596e29996c3a0f98154f08becf57da61
Jun Shao <mr.shaojun@gmail.com>
2024-05-10 20:53:46 +1000

    change task status when require client for action

--- zwf-web/src/components/showRequireActionModal.js
+++ zwf-web/src/components/showRequireActionModal.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Modal, Typography, Input, Row, Space, Avatar, Button, Form } from 'antd';
 import { TaskIcon } from 'components/entityIcon';
-import { getTaskDeepLinkUrl, notifyTask$, renameTask$ } from 'services/taskService';
+import { changeTaskStatus$, getTaskDeepLinkUrl, notifyTask$, renameTask$ } from 'services/taskService';
 import { ClickToCopyTooltip } from './ClickToCopyTooltip';
 import Icon, { NotificationOutlined, ShareAltOutlined } from '@ant-design/icons';
 import { MdDriveFileRenameOutline } from 'react-icons/md'
+import { combineLatest } from 'rxjs';
 
 const { Text, Paragraph } = Typography;
 
@@ -13,7 +14,9 @@ const Content = props => {
   const formRef = React.createRef();
 
   const handleSendNotification = (values) => {
-    notifyTask$(id, values.message).subscribe(() => {
+    const message$ = notifyTask$(id, values.message);
+    const status$ = changeTaskStatus$(id, 'action_required');
+    combineLatest([message$, status$]).subscribe(() => {
       onOk();
     })
   }
@@ -27,17 +30,17 @@ const Content = props => {
       style={{ marginTop: 20 }}
     >
       <Form.Item name="message" rules={[{ required: false, max: 1000, message: ' ', whitespace: false }]}>
-        <Input.TextArea 
-        placeholder="Leave additional message or explanation of the request."
-        autoSize={{minRows: 3}}
-        autoFocus 
-        allowClear 
-        maxLength={1000} 
-        showCount 
+        <Input.TextArea
+          placeholder="Leave additional message or explanation of the request."
+          autoSize={{ minRows: 3 }}
+          autoFocus
+          allowClear
+          maxLength={1000}
+          showCount
         />
       </Form.Item>
     </Form>
-    <Row justify="end" style={{marginTop: 32}}>
+    <Row justify="end" style={{ marginTop: 32 }}>
       <Button type="primary" onClick={() => formRef.current.submit()}>Send Notification Email</Button>
     </Row>
   </>
