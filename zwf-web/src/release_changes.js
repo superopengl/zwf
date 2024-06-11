2257c2eeaa72364a2c26b1a9b2c892053d0e22bd
Jun Shao <mr.shaojun@gmail.com>
2024-06-11 21:19:10 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,10 +1,2 @@
 export const notes=`
-federal login test
-set orgId when sign up org trial user
-send email when register with an existing email
-daily subscription app
-add admin_expired and agent_expired roles
-typeorm fix
-add bulk enqueue email function
-update typeorm usage
-fix table entity`
+federal login trial 2`
