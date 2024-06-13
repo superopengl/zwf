6b37a7d98f39d91b95e410c7eb950e99b87deda4
Jun Shao <mr.shaojun@gmail.com>
2024-06-13 21:52:37 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,3 +1,7 @@
 export const notes=`
-update release notes
-federal login set sameSite='none' attempt`
+federated login test
+refine user login
+tidy up table schema
+remove federal login code
+cleanup unread message count
+fix org admin onboard flow`
