41b7579a0a1ae95f53eedea4bb2a409828ab99a4
Jun Shao <mr.shaojun@gmail.com>
2024-06-25 07:12:27 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,2 +1,8 @@
 export const notes=`
-update release notes`
+UX refine for smart search
+go to / for unknown route path
+clean up
+more rxjs usage
+code clean up
+save task template bugfix
+save doc template bugfix`
