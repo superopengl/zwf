2e4377c08243808288b8ea8b21cb07b104a0dd1c
Jun Shao <mr.shaojun@gmail.com>
2024-08-09 15:45:53 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,9 +1,2 @@
 export const notes=`
-update release notes
-home page ui refine
-home menu refine
-refine org signup flow
-fix subscription
-fix free trial provisioning
-new payment implementation
-placeholder prop`
+temp fix for subscription cron`
