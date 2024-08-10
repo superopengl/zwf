425a99764c37967078a3464ea7d1ffa6ae26d47e
Jun Shao <mr.shaojun@gmail.com>
2024-08-11 01:51:42 +1000

    tweak price card

--- zwf-web/src/components/homeAreas/HomePricingArea.js
+++ zwf-web/src/components/homeAreas/HomePricingArea.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Spin, Typography, Col, Row } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
+import { Card, Typography, Col, Row, Space, Button } from 'antd';
+import { CheckCircleOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 import { SubscriptionCard } from 'components/SubscriptionCard';
 import { GiCurvyKnife, GiFireAxe, GiSawedOffShotgun, GiPirateCannon } from 'react-icons/gi';
@@ -9,7 +9,7 @@ import { VscRocket } from 'react-icons/vsc';
 import { AiOutlineHome } from 'react-icons/ai';
 import { subscriptionDef } from 'def/subscriptionDef';
 
-const { Title, Paragraph } = Typography;
+const { Title, Paragraph, Text } = Typography;
 
 const StyledRow = styled(Row)`
 `;
@@ -32,17 +32,31 @@ const Container = styled.div`
 justify-content: center;
 margin-bottom: 6rem;
 width: 100%;
-text-align: center;
-padding: 4rem 1rem;
+
+// text-align: center;
 // background: #fafafa;
 // background: rgb(240, 242, 245);
 `;
 
 const InnerContainer = styled.div`
-margin-left: auto;
-margin-right: auto;
 width: 100%;
-max-width: 900px;
+background-color:  #F1F2F5;
+padding-top: 3rem;
+.price-card {
+  max-width: 400px;
+  background: #001A1F;
+  box-shadow: 0px 0px 24px rgba(0, 26, 31, 0.36);
+  backdrop-filter: blur(20px);
+  border-radius: 12px;
+  padding: 24px;
+  color: #EBEDF1CC;
+  font-weight: 300;
+
+  .ant-typography {
+    color: #EBEDF1CC;
+    font-weight: 300;
+  }
+}
 `;
 
 
@@ -51,21 +65,33 @@ export const HomePricingArea = props => {
   return (
     <Container>
       <InnerContainer>
-        <Title>Choose the plan that's right for you</Title>
-        <Paragraph type="secondary">
-        Subscription plans start at $19.00 / month per person
-        </Paragraph>
-        <StyledRow gutter={[40, 40]}>
-          {subscriptionDef.map(s => <StyledCol key={s.key} {...span}>
-            <SubscriptionCard
-              title={s.title}
-              icon={s.icon}
-              description={s.description}
-              price={s.price}
-              interactive={false}
-              unit={s.unit} />
-          </StyledCol>)}
-        </StyledRow>
+        <Title style={{ color: '#0FBFC4', textAlign: 'center', fontWeight: 700, fontSize: 18, lineHeight: 1.5, margin: 0 }}>Only One Plan !</Title>
+        <Title style={{ textAlign: 'center', fontWeight: 800, fontSize: 36, lineHeight: 1, margin: 0 }}>We get tired of multiple plans</Title>
+        <Row>
+          <Col flex="auto"></Col>
+          <Col flex="none" style={{ position: 'relative', top: 48 }}>
+            <div className='price-card'>
+              <Space direction='vertical' style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ color: '#0FBFC4', fontSize: 28, lineHeight: 1.2, fontWeight: 800 }}>ALL IN ONE PLAN</Text>
+                <div><Text style={{ color: '#ffffff', fontSize: 36, fontWeight: 800 }}>$39.0</Text><Text style={{ color: '#97A3B7' }}> <small>/Month</small></Text></div>
+              </Space>
+              <Paragraph style={{ fontSize: 16, marginTop: '1rem' }}>
+                Description description description description description description description description description description description description description description
+              </Paragraph>
+              <Paragraph>
+                <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> All the features we have
+              </Paragraph>
+              <Paragraph>
+                <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> 15 days free trial
+              </Paragraph>
+              <Paragraph>
+                <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> Monthly auto renew payment
+              </Paragraph>
+              <Button type="primary" size="large" block style={{ margin: '1rem 0 0' }}>Join Now</Button>
+            </div>
+          </Col>
+          <Col flex="auto"></Col>
+        </Row>
       </InnerContainer>
     </Container>
   )
