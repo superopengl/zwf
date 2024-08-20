4b12d9677f4ff93ed085623d4c738112109e3a3c
Jun Shao <mr.shaojun@gmail.com>
2024-08-20 20:44:40 +1000

    clean up logos

--- zwf-web/src/components/Logo.js
+++ zwf-web/src/components/Logo.js
@@ -1,10 +1,9 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logoSvg from '../logo-blue.svg';
 import {Image} from 'antd';
 
 export const Logo = (props) =>
   <Link to="/">
-    <Image src={logoSvg} alt="logo" preview={false} width={props.size || 80} />
+    <Image src="/images/logo.svg" alt="logo" preview={false} width={props.size || 60} />
   </Link>
