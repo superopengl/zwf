87a36fe62fb37ebab5591caf20468f0182d260ad
Jun Shao <mr.shaojun@gmail.com>
2024-08-21 21:34:46 +1000

    tinymce variable plugin
    https://github.com/ziktar/tinymce-variable

---
+++ zwf-web/public/tinymce/plugins/variable/index.js
@@ -0,0 +1,7 @@
+// Exports the "autolink" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/autovar')
+//   ES2015:
+//     import 'tinymce/plugins/autovar'
+require('./plugin.js');
