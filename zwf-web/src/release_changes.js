23aecad998d77624d5249304c63e3d3bbf041ffd
Jun Shao <mr.shaojun@gmail.com>
2024-08-22 00:16:00 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,2 +1,14 @@
 export const notes=`
-update release notes`
+defensive fix
+adding loading when Google SSO
+terminate subscription logic
+tweak standalone pages
+adjust body color
+fix 14 day free trial period
+fix selection
+no language on profile form
+tinymce variable font size fix
+add variable plugin in to tinymce
+tinymce variable plugin
+add tinymce source to git
+change default color`
