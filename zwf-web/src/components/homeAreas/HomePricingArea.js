e79526a093d1f7e163377920003916cfac551281
Jun Shao <mr.shaojun@gmail.com>
2024-08-23 17:45:52 +1000

    homepage ui refine

--- zwf-web/src/components/homeAreas/HomePricingArea.js
+++ zwf-web/src/components/homeAreas/HomePricingArea.js
@@ -41,11 +41,12 @@ width: 100%;
 
 const InnerContainer = styled.div`
 width: 100%;
-// background-color:  #F1F2F5;
-background-image: linear-gradient(5deg, #F1F2F5, #F1F2F5 50%, #ffffff 50%, #ffffff 100%);
-padding-top: 3rem;
+background-color:  #F6F7F9;
+// background-image: linear-gradient(5deg, #F1F2F5, #F1F2F5 50%, #ffffff 50%, #ffffff 100%);
+padding-top: 6rem;
 .price-card {
   max-width: 400px;
+  width: calc(100vw - 2rem);
   background: #001A1F;
   box-shadow: 0px 0px 24px rgba(0, 26, 31, 0.36);
   backdrop-filter: blur(20px);
@@ -59,6 +60,7 @@ padding-top: 3rem;
     font-weight: 300;
   }
 }
+
 `;
 
 
@@ -78,70 +80,56 @@ export const HomePricingArea = props => {
 
   return (
     <Container>
-      <InnerContainer>
-        <Title style={{ color: '#0FBFC4', textAlign: 'center', fontWeight: 700, fontSize: 18, lineHeight: 1.5, margin: 0 }}>Only One Plan !</Title>
-        <Title style={{ textAlign: 'center', fontWeight: 800, margin: 0 }}>We get tired of multiple plans</Title>
-        <Row align='end'
-          justify='center'
-          gutter={20}
-          style={{ alignItems: 'flex-end' }}
-        >
-          <Col flex="auto" style={{ textAlign: 'right', }}
-            {...{
-              xs: 0,
-              sm: 0,
-              md: 0,
-              lg: 6,
-              xl: 8,
-              xxl: 8
-            }}
+      <InnerContainer justify='center'>
+        <Title style={{ color: '#0FBFC4', textAlign: 'center', fontWeight: 700, fontSize: 18, lineHeight: 2, margin: 0 }}>We get tired of multiple plans!</Title>
+        <Title style={{ textAlign: 'center', fontWeight: 800, margin: 0 }}>Hassle-Free with <Text style={{ color: '#0FBFC4' }}>Just one plan</Text></Title>
+        <Row justify='center'>
+          <Row align='end'
+            justify='center'
+            gutter={20}
+            wrap={false}
+            style={{ alignItems: 'flex-end' }}
           >
-            <Image src="/images/price-card-left.svg" preview={false} />
-          </Col>
-          <Col
-            style={{ position: 'relative', top: 48, display: 'flex', justifyContent: 'center' }}
-            {...{
-              xs: 24,
-              sm: 24,
-              md: 24,
-              lg: 12,
-              xl: 8,
-              xxl: 8
-            }}
-          >
-            <div className='price-card'>
-              <Space direction='vertical' style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: '#0FBFC4', fontSize: 28, lineHeight: 1.2, fontWeight: 800 }}>ALL IN ONE PLAN</Text>
-                <div><Text style={{ color: '#ffffff', fontSize: 36, fontWeight: 800 }}>$39.0</Text><Text style={{ color: '#97A3B7' }}> <small>/Month</small></Text></div>
-              </Space>
-              <Paragraph style={{ fontSize: 16, marginTop: '1rem' }}>
-                Description description description description description description description description description description description description description description
-              </Paragraph>
-              <Paragraph>
-                <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> All the features we have
-              </Paragraph>
-              <Paragraph>
-                <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> 14 days free trial
-              </Paragraph>
-              <Paragraph>
-                <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> Monthly auto renew payment
-              </Paragraph>
-              <Button type="primary" size="large" block style={{ margin: '1rem 0 0' }} onClick={handleShowModal} >Join Now</Button>
-            </div>
-          </Col>
-          <Col flex="auto"
-            {...{
-              xs: 0,
-              sm: 0,
-              md: 0,
-              lg: 6,
-              xl: 8,
-              xxl: 8
-            }}
-          >
-            <Image src="/images/price-card-right.svg" preview={false} />
+            <Col flex="auto" style={{ textAlign: 'right', width: 400 }}
+            >
+              <Image src="/images/price-card-left.svg" preview={false} />
+            </Col>
+            <Col
+              style={{ position: 'relative', top: 48, display: 'flex', justifyContent: 'center', minWidth: 360 }}
+            >
+              <div className='price-card'>
+                <Space direction='horizontal' align="end" style={{ width: '100%', justifyContent: 'center'}}>
+                  <Text style={{ color: '#0FBFC4', fontSize: 28, lineHeight: 1.2, fontWeight: 800 }}>ALL IN ONE PLAN</Text>
+                  <Space><Text style={{ color: '#ffffff', fontSize: 36, fontWeight: 800 }}>$39.0</Text><Text style={{ color: '#97A3B7' }}> <small>/Month</small></Text></Space>
+                </Space>
+                <Paragraph style={{ fontSize: 16, marginTop: '1rem' }}>
+                  Description description description description description description description description description description description description description description
+                </Paragraph>
+                <Paragraph>
+                  <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> 14 days free trial
+                </Paragraph>
+                <Paragraph>
+                  <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> All the features included
+                </Paragraph>
+                <Paragraph>
+                  <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> Coming-up features always included
+                </Paragraph>
+                <Paragraph>
+                  <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> Monthly auto renew payment
+                </Paragraph>
+                <Paragraph>
+                  <CheckCircleOutlined style={{ color: '#009A29', marginRight: '1rem' }} /> No long term contract bound
+                </Paragraph>                
+                <Button type="primary" size="large" block style={{ margin: '1rem 0 0' }} onClick={handleShowModal} >Join Now</Button>
+              </div>
+            </Col>
+            <Col flex="auto"
+              style={{ textAlign: 'left',width: 400 }}
+            >
+              <Image src="/images/price-card-right.svg" preview={false} />
 
-          </Col>
+            </Col>
+          </Row>
         </Row>
         <OrgRegisterModal
           visible={visible}
