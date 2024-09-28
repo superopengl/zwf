e8f22826ad94aa2fccb2e97f59c73c42650a5354
Jun Shao <mr.shaojun@gmail.com>
2024-09-28 17:26:54 +1100

    refactor

--- zwf-app/src/utils/createNewTicketForUser.ts
+++ zwf-app/src/utils/createNewTicketForUser.ts
@@ -1,21 +1,18 @@
+import { OrgBasicInformation } from './../entity/views/OrgBasicInformation';
 import { EntityManager, MoreThan } from 'typeorm';
-import { getRecurring } from './../api/recurringController';
 import { LicenseTicket } from '../entity/LicenseTicket';
 import { assert } from './assert';
 import { getCurrentPricePerSeat } from './getCurrentPricePerSeat';
 import { OrgPromotionCode } from '../entity/OrgPromotionCode';
-import { db } from '../db';
 import { getUtcNow } from './getUtcNow';
-import { Org } from '../entity/Org';
-import moment = require('moment');
 
 
 export async function createNewTicketForUser(m: EntityManager, userId: string, orgId: string) {
   assert(userId, 500, 'userId is null or empty');
   assert(orgId, 500, 'userId is null or empty');
 
-  const org = await m.getRepository(Org).findOneBy({id: orgId});
-  const type = moment(org.trialEndsTill).isAfter() ? 'trial' : 'paid'; 
+  const org = await m.getRepository(OrgBasicInformation).findOneBy({id: orgId});
+  const type = org.isInTrial ? 'trial' : 'paid'; 
 
   const ticket = new LicenseTicket();
   ticket.orgId = orgId;
