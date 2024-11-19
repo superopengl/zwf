cbbb094d6bab7be21c6c06c46d1295d5311cc981
Jun Shao <mr.shaojun@gmail.com>
2024-11-20 05:28:16 +1100

    bugfix for version mismatch notification

--- zwf-web/src/components/showVersionMismatchAlert.js
+++ zwf-web/src/components/showVersionMismatchAlert.js
@@ -3,9 +3,5 @@ import { BehaviorSubject } from 'rxjs';
 export const versionMismatchSubject$ = new BehaviorSubject();
 
 export const showVersionMismatchAlert = (webappVersion, backendVersion) => {
-  if (webappVersion === backendVersion) {
-    return;
-  }
-
   versionMismatchSubject$.next(backendVersion);
 }
