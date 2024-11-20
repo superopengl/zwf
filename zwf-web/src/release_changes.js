136d546453be245f0639e3b9d3e11caba31f9b9c
Jun Shao <mr.shaojun@gmail.com>
2024-11-21 05:34:55 +1100

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,3 +1,12 @@
 export const notes=`
-update release notes
-tweak anchor`
+bugfix for version mismatch notification
+refine payment flow
+change receipt to use new logo
+refactor
+more refine
+more fix
+more implementation for tickets.
+remove credit
+remove subscription
+bugfix
+license ticket`
