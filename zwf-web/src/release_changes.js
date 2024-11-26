9ea58f45c45ef10ed22af713538c7a3e6f3ea453
Jun Shao <mr.shaojun@gmail.com>
2024-11-26 11:14:37 +1100

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,9 +1,2 @@
 export const notes=`
-update release notes
-pnpm fetch and monorepo
-fix Dockerfile
-successful pnpm fetch
-update web pnpm lock file
-rename web
-refine
-fix branding`
+refine ignore files`
