d55f4777fa9fa6c7dc20b11b8ddb4a63718f8a9f
Jun Shao <mr.shaojun@gmail.com>
2025-02-12 20:02:59 +1100

    auth check - org member must have orgId

--- zwf-app/src/utils/assertRole.ts
+++ zwf-app/src/utils/assertRole.ts
@@ -1,11 +1,36 @@
 import { Role } from '../types/Role';
 import { assert } from './assert';
+import { getOrgIdFromReq } from './getOrgIdFromReq';
+import { getRoleFromReq } from './getRoleFromReq';
 
 
 export function assertRole(req, ...roles) {
   assert(req?.user, 401, 'Session timeout');
   if (roles && roles.length) {
-    const { role } = req?.user;
-    assert(roles.includes(role), 403, `Invalid permission ('${role}' is to access '${roles.join()}')`);
+    const reqRole = getRoleFromReq(req);
+    const reqOrgId = getOrgIdFromReq(req);
+    let canAccess = false;
+    for (const allowedRole of roles) {
+      switch (allowedRole) {
+        case Role.Admin:
+        case Role.Agent:
+          // Org members must have orgId.
+          canAccess = reqRole === allowedRole && !!reqOrgId;
+          break;
+        case Role.System:
+        case Role.Client:
+        case Role.Guest:
+        default:
+          // Non org members must not have orgId.
+          canAccess = reqRole === allowedRole && !reqOrgId;
+          break;
+      }
+
+      if (canAccess) {
+        break;
+      }
+    }
+
+    assert(canAccess, 403, `Invalid permission ('${reqRole}' is to access '${roles.join()}')`);
   }
 }
