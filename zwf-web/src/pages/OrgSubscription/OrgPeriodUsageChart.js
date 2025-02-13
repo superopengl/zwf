eaea0070f9f9c161777f425f58077309734c5af7
Jun Shao <mr.shaojun@gmail.com>
2025-02-14 05:24:38 +1100

    refine usage chart

--- zwf-web/src/pages/OrgSubscription/OrgPeriodUsageChart.js
+++ zwf-web/src/pages/OrgSubscription/OrgPeriodUsageChart.js
@@ -1,29 +1,12 @@
-import { Card, Button, Modal, Space, Typography, Tag, List, Descriptions, Row, Col } from 'antd';
 import React from 'react';
 
 import { Loading } from 'components/Loading';
-import { CloseCircleFilled, CloseOutlined, LeftOutlined, PlusOutlined, RightOutlined } from '@ant-design/icons';
-import { deleteOrgPaymentMethod$, listOrgPaymentMethods$, setOrgPrimaryPaymentMethod$ } from 'services/orgPaymentMethodService';
-import StripeCardPaymentWidget from 'components/checkout/StripeCardPaymentWidget';
-import { saveOrgPaymentMethod$ } from 'services/orgPaymentMethodService';
-import { PageContainer, ProCard, ProList } from '@ant-design/pro-components';
-import styled from 'styled-components';
-import { useNavigate, Link } from 'react-router-dom';
-import { finalize, switchMap } from 'rxjs';
-import { useAddPaymentMethodModal } from 'components/useAddPaymentMethodModal';
+import { finalize } from 'rxjs';
 import { getPeriodUsage$ } from '../../services/billingService';
-import { DebugJsonPanel } from 'components/DebugJsonPanel';
 import { Bar } from '@ant-design/plots';
 import PropTypes from 'prop-types';
 import { getUserDisplayName } from 'util/getUserDisplayName';
 import moment from 'moment';
-
-const { Text, Paragraph } = Typography;
-
-const Container = styled.div`
-display: flex;
-justify-content: center;
-`
 
 const convertToDate = (m) => {
   return moment(m).format('DD/MMM');
@@ -31,10 +14,9 @@ const convertToDate = (m) => {
 
 export const OrgPeriodUsageChart = React.memo((props) => {
 
-  const { id: periodId, periodFrom, periodTo, periodDays } = props.period;
+  const { id: periodId, periodFrom, periodDays } = props.period;
 
   const [loading, setLoading] = React.useState(true);
-
   const [list, setList] = React.useState([]);
 
   React.useEffect(() => {
@@ -64,17 +46,6 @@ export const OrgPeriodUsageChart = React.memo((props) => {
     }))
   }
 
-  // const data = [
-  //   {
-  //     type: '分类一',
-  //     values: [76, 100],
-  //   },
-  //   {
-  //     type: '分类二',
-  //     values: [56, 108],
-  //   },
-  // ];
-
   const config = {
     data: list,
     xField: 'usage',
@@ -83,41 +54,46 @@ export const OrgPeriodUsageChart = React.memo((props) => {
     xAxis: {
       min: 0,
       max: periodDays,
-      // minLimit: 0,
-      // maxLimit: 31,
       grid: {
-
         line: {
           style: {
-            lineDash: [2, 2]
-            
+            lineDash: [2, 2],
+            strokeOpacity: 0.7,
           }
         },
       },
       tickInterval: 1,
       label: {
         formatter: (text, item, index) => {
+          const time = moment(rangeFrom).add(+text, 'day');
           if (index === 0 || index === periodDays - 1) {
-            return moment(+text).format('MMM D YYYY')
+            return time.format('MMM D YYYY')
           } else {
-            return moment(+text).format('D')
+            return time.format('MMM D')
           }
         }
       }
     },
     isRange: false,
     color: '#0FBFC4',
-    width: 700,
-    maxBarWidth: 8,
+    autoFit: true,
+    padding: 'auto',
+    maxBarWidth: 24,
+    barStyle: {
+      fill: 'l(0) 0:#0FBFC4 1:#0051D9',
+      shadowColor: '#999999',
+      shadowBlur: 10,
+      shadowOffsetX: 2,
+      shadowOffsetY: 2,
+    },
     meta: {
       usage: {
-        min: 0, //rangeFrom,
-        max: periodDays, //rangeTo,
-        // range: [rangeFrom, rangeTo]
+        min: 0,
+        max: periodDays
       }
     },
     tooltip: {
-      customContent: (text, item, index) => {
+      customContent: (text, item) => {
         const rawData = item?.[0]?.data?.raw;
         if (!rawData) return null;
         const { ticketFrom, ticketTo, ticketDays } = rawData;
@@ -125,8 +101,8 @@ export const OrgPeriodUsageChart = React.memo((props) => {
       }
     },
     label: {
-      position: 'top',
-      formatter: (item, other) => {
+      position: 'middle',
+      formatter: (item) => {
         return item.raw.ticketDays;
       },
       layout: [
@@ -140,7 +116,7 @@ export const OrgPeriodUsageChart = React.memo((props) => {
   return (
     <Loading loading={loading} >
       <Bar {...config} />
-      <DebugJsonPanel value={list} />
+      {/* <DebugJsonPanel value={list} /> */}
     </Loading>
   );
 });
