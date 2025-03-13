202cfb6fc3882b24d1eefafd82fbaf5562058b3a
Jun Shao <mr.shaojun@gmail.com>
2025-03-13 23:59:11 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,12 +1,3 @@
 export const notes=`
-not auto sync db when launching app
-wording
-remember view mode in local storage
-able to update tags from task list view
-task board ui refine
-fix React warning
-hide list view for form template list page
-empty locale
-empty text for scheduler list
-refine placeholder ui
-refine task card`
+switch prod
+more fix`
