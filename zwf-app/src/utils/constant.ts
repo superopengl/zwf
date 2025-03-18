66e5fa3d6b1930af0bfe6b8fa51f4ced1b410324
Jun Shao <mr.shaojun@gmail.com>
2025-03-19 05:57:35 +1000

    change bcc to info@

--- zwf-app/src/utils/constant.ts
+++ zwf-app/src/utils/constant.ts
@@ -1,2 +1,2 @@
 export const SYSTEM_EMAIL_SENDER = 'ZeeWorkflow <noreply@zeeworkflow.com>';
-export const SYSTEM_EMAIL_BCC = 'jshao <jshao@zeeworkflow.com>';
+export const SYSTEM_EMAIL_BCC = 'ZeeWorkflow Team <info@zeeworkflow.com>';
