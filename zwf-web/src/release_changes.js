312ab0fcd49d616c0a0c52624fd7daaf0cc893dd
Jun Shao <mr.shaojun@gmail.com>
2025-04-07 00:04:30 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,4 +1,17 @@
 export const notes=`
-fix missing payment method prompt
-log message fix
-fix error duration`
+no tu and pp on sider footer
+fix member select
+more client name card fix
+fix client name cards in recurring
+fix client name cards
+org client fix
+refine click to edit input
+change client alias
+name card with alias
+user tags moved to org client
+able to add client without email
+tag select refine
+db schema fix
+fix login error notification bug
+orgClient can be without userId
+typo fix`
