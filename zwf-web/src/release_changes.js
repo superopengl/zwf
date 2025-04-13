9457e56b65946821ff65c145bf8cf27ffa28e206
Jun Shao <mr.shaojun@gmail.com>
2025-04-14 07:52:51 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,5 +1,11 @@
 export const notes=`
-style bugfix
-client able to drop files
-drop file refine
-bugfix`
+more fix
+doc_template => demplate (doc template)
+task_template => femplate (form template)
+form editor bugfix
+able to add template from form template select
+clean up
+refine field editor
+fix control grab
+fix control drag
+fix float panel`
