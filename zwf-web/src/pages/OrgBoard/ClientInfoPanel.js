64e952818122ecfd418ca7336a0577fc1366e118
Jun Shao <mr.shaojun@gmail.com>
2025-04-15 18:54:23 +1000

    zwf account tooktip

--- zwf-web/src/pages/OrgBoard/ClientInfoPanel.js
+++ zwf-web/src/pages/OrgBoard/ClientInfoPanel.js
@@ -1,7 +1,7 @@
 import { Button } from 'antd';
 import React from 'react';
 import { useAssertRole } from 'hooks/useAssertRole';
-import { Drawer, Form, Typography, Input, Descriptions } from 'antd';
+import { Drawer, Tooltip, Typography, Space, Descriptions } from 'antd';
 import PropTypes from 'prop-types';
 import { ClientNameCard } from 'components/ClientNameCard';
 import { finalize } from 'rxjs';
@@ -11,6 +11,7 @@ import { getOrgClientDatabag$, getOrgClientInfo$, saveOrgClientEmail$ } from 'se
 import { notify } from 'util/notify';
 import { InviteClientInput } from 'components/InviteClientInput';
 import { TimeAgo } from 'components/TimeAgo';
+import { CheckCircleOutlined } from '@ant-design/icons';
 
 const { Text } = Typography;
 
@@ -22,11 +23,19 @@ export const ClientInfoPanel = (props) => {
 
   const profile = orgClient?.user?.profile;
 
+  const isZwfAccount = !!(profile?.email);
   return (
     <>
       <Descriptions column={1} bordered={false} size="small" layout="horizontal">
-        <Descriptions.Item label="Email">{profile?.email}</Descriptions.Item>
-         <Descriptions.Item label="Invited from"><TimeAgo value={orgClient.createdAt} /></Descriptions.Item>
+        <Descriptions.Item label="Email">
+          <Space>
+            {profile?.email}
+            {isZwfAccount && <Tooltip title="Verified ZeeWorkflow account" placement="bottom">
+             <Text type="success"><CheckCircleOutlined/></Text> 
+            </Tooltip>}
+          </Space>
+        </Descriptions.Item>
+        <Descriptions.Item label="Invited from"><TimeAgo value={orgClient.createdAt} /></Descriptions.Item>
         {/*<Descriptions.Item label="Surname">{profile?.surname}</Descriptions.Item>
         <Descriptions.Item label="Phone">{profile?.phone}</Descriptions.Item> */}
       </Descriptions>
