6edfec4a2d1be85a12eb8f759d68c9ad72de545a
Jun Shao <mr.shaojun@gmail.com>
2025-04-19 22:33:45 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,2 +1,31 @@
 export const notes=`
-zwf account tooktip`
+misc
+wording fix
+ui refine
+gutter refine
+bugfix task rename
+refactoring
+misc
+client mobile view refine
+ui refine
+fix fixHeaader issue
+client page mobile view
+ui refine
+ui refine
+client glowing
+glow
+fix client view
+bugfix
+client 1 to many mapping to users
+wording
+notification
+refine notification db table
+refine
+ui refine
+notification bar
+watchlist
+fix task board update
+misc fix
+task events
+avoid hardcoded schema
+misc`
