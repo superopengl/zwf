aedf3bdafdf74807d240dfe49a9b618d2853bdf7
Jun Shao <mr.shaojun@gmail.com>
2025-05-01 17:43:58 +1000

    update release notes

--- zwf-web/src/release_changes.js
+++ zwf-web/src/release_changes.js
@@ -1,4 +1,17 @@
 export const notes=`
-hide side menu for clients
-fix header
-ui refine`
+fix download task doc
+accurate price
+form badge
+remove server end zevent filter
+refine view
+refactor zevent badge
+refactoring
+zindex fix
+fix padding
+scrolling fix
+hide support for clients
+hide notification for clients
+bugfix
+zevent refine
+code refactoring
+client ui refine`
