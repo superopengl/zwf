1ed713095f573a765d8335f1bb1e0bf39287a6fc
Jun Shao <mr.shaojun@gmail.com>
2025-05-01 21:11:50 +1000

    able to mention team member

--- zwf-web/src/services/taskCommentService.js
+++ zwf-web/src/services/taskCommentService.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL, httpPost$, httpGet$ } from 'services/http';
 
-export function addTaskComment$(taskId, message) {
-  return httpPost$(`task/${taskId}/comment`, { message });
+export function addTaskComment$(taskId, message, mentionedUserIds) {
+  return httpPost$(`task/${taskId}/comment`, { message, mentionedUserIds });
 }
 
 
